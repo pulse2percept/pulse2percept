1c0e9331718bc3686938c5ff0dc8cdd90dd09f98
arokem <arokem@gmail.com>
2016-01-12 12:40:40 -0800

    Implement the repr.

--- python/utils.py
+++ python/utils.py
@@ -7,3 +7,7 @@ class Parameters(object):
     def __init__(self, **params):
         for k, v in params.items():
             self.__dict__[k] = v
+
+    def __repr__(self):
+        for k, v in params.items():
+            print("%s : %s" % (k, v))
