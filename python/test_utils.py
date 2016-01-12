655f97a8ecb9d3adabfc2318c5c93aa83d2711d0
arokem <arokem@gmail.com>
2016-01-12 12:36:31 -0800

    Formatting.

--- python/test_utils.py
+++ python/test_utils.py
@@ -2,6 +2,6 @@ import utils
 import numpy.testing as npt
 
 def test_Parameters():
-    my_params = utils.Parameters(foo='bar', list=[1,2,3])
+    my_params = utils.Parameters(foo='bar', list=[1, 2, 3])
     assert my_params.foo == 'bar'
     assert my_params.list == [1, 2, 3]
