67b0559c4ec91a69845f9a0a7fcc2151b62b18e7
Ione Fine <ionefine@uw.edu>
2016-01-14 14:54:10 -0800

    This is the working Alan/Dev model converting a 1D vector of effective current for a point on the retina into brightness. Includes the Devyani modification. Needs to be 'functionized' which will require 3 things. 1. move the stimulus generation code into a separate little utility (since it will be useful later). Move gamma_gmb (which is currently a subfunction) somewhere useful (3) turn the remainder into a function that should have stim (1D 'object' that contains tsform and tsample and an output that just contains R4 (which could be renamed brightness?

--- python/effectivecurrent2brightness.py
+++ python/effectivecurrent2brightness.py
@@ -16,14 +16,15 @@ import utils
 dur=.5
 tsample=.01/1000;
 
-stim = utils.Parameters(freq=20, dur=.5,pulsedur=.075/1000, amp=5, tsample=.075/1000) 
+stim = utils.Parameters(freq=20, dur=.5,pulsedur=.075/1000, 
+                        amp=20, tsample=.075/1000, amplitude=10) 
 stim.t =np.arange(0, stim.dur, stim.tsample)# a vector of time
 sawtooth=stim.freq * np.mod(stim.t, 1/stim.freq)
 on=np.logical_and(sawtooth>(stim.pulsedur*stim.freq), 
                   sawtooth< (2 * stim.pulsedur * stim.freq))
 on=on.astype(float);
 off=sawtooth<stim.pulsedur * stim.freq
-stim.tsform = on.astype(float)-off.astype(float)
+stim.tsform =stim.amplitude *( on.astype(float)-off.astype(float))
 
 def gamma_gmb(n,tau,t):
     """y=Gamma(n,theta,t)
@@ -47,39 +48,56 @@ def gamma_gmb(n,tau,t):
     
 
 # model parameters
-p=utils.Parameters(tau1 = .42/1000, tau2 = 45.25/1000, tau3 =  26.25/1000, e = 8.73, beta = .6)
+p=utils.Parameters(tau1 = .42/1000, tau2 = 45.25/1000, tau3 =  26.25/1000, 
+                   e = 2.25, beta = .6
+                   ,asymptote=14,   slope=.3, shift=47)
 # p.tau1 = .42/1000  # fast leaky integrator, from Alan model, tends to be between .24 - .65
 # p.tau2 = 45.25/1000   integrator for charge accumulation, has values between 38-57
-# p.e = 8.73  scaling factor for the effects of charge accumulation 2-3 for threshold or 8-10 for suprathreshold
+# p.e = scaling factor for the effects of charge accumulation 2-3 for threshold or 8-10 for suprathreshold
 # p.tau3 =  26.25/1000 # 24-33 FIX
 
-# p.beta = .6  #3-4.2 for threshold or .6-1 for suprathreshold
-## beta = 3.5; FIX, will need to be replaced with Devyani's nonlinearity
+# parameters for a stationary nonlinearity providing a continuous functino
+# that nonlinearly rescales the response 
+#based on Nanduri et al 2012, equation 3
+#p.asymptote = 14 
+# p.slope =.3
+# p.shift =47 
 
-
+# create all the leaky integrators we are going to use
 # implement a fast leaky integrator
 t=np.arange(0,20*p.tau1, stim.tsample)
 G1=gamma_gmb(1, p.tau1, t) #% fast impulse response function
-R1 = stim.tsample*np.convolve(G1,stim.tsform)
-#R1 = R1[0:len(stim.t)] #cut off extra stuff at the end due to convolution
+
 
 # implement a slightly slower leaky integrator, which is used to model adapatation due to charge accumulation over time
 t = np.arange(0, 8*p.tau2,stim.tsample) # this could be shrunk slightly if need be
 G2 = gamma_gmb(1, p.tau2, t)
 
-rect_tsform=stim.tsform>0 # rectify the stimulation time course
-ca=stim.tsample *np.cumsum(rect_tsform.astype(float))
-chargeaccumulated=p.e * stim.tsample*np.convolve(G2, ca)
-#chargeaccumulated=chargeaccumulated[0:len(stim.t)]
-
-R1=np.concatenate([R1, np.zeros(len(chargeaccumulated)-len(R1))])
-
-R2=R1-chargeaccumulated
-
-#this section will need to be updated with Devyani's equation
-R3=(R2>0)**p.beta
-
 # implement a final slow leaky integrator for the brain
 t= np.arange(0, p.tau3*8, stim.tsample) # this is cropped as tightly as possible for speed sake
 G3 = gamma_gmb(3,p.tau3,t)
+
+# implement the nonlinear model
+# charge integration in the cell
+R1 = stim.tsample*np.convolve(G1,stim.tsform)
+
+# use a rectified version of the stimulation time course
+# to calculate charge accumulation, and subtract accumulated charge
+rect_tsform=stim.tsform;
+ind=stim.tsform<0
+rect_tsform[ind]=0;
+ca=stim.tsample *np.cumsum(rect_tsform.astype(float))
+chargeaccumulated=p.e * stim.tsample*np.convolve(G2, ca)
+R1=np.concatenate([R1, np.zeros(len(chargeaccumulated)-len(R1))])
+R2=R1-chargeaccumulated
+ind=R2<0
+R2[ind]=0 # rectify again
+
+# now we put in the stationary nonlinearity of Devyanis
+R2norm=R2 / R2.max() # normalize
+scFac=p.asymptote/(1+np.exp(-(R2/p.slope)+p.shift))
+R3=R2norm*scFac #scaling factor varies with original amplitude
+
+R4=stim.tsample* np.convolve(G3, R3)
+
 R4 =stim.tsample*np.convolve(G3,R3)
