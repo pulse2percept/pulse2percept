145ad7c417ddf54ff40a6f5836deadda9cdb8cad
Geoff Boynton <gboynton@uw.edu>
2016-01-21 16:25:40 -0800

    Added sparseconv function to the utils file

--- python/utils.py
+++ python/utils.py
@@ -1,6 +1,7 @@
 """
 Utility functions for pulse2percept
 """
+import numpy as np
 
 
 class Parameters(object):
@@ -18,3 +19,31 @@ class Parameters(object):
 
     def __setattr(self, name, value):
         self.__dict__[name] = values
+
+def sparseconv(v,a):
+    """Returns the discrete, linear convolution of two one-dimensional sequences.
+       output is of length v
+       
+       Runs faster than numpy.convolve if:
+       (1) a is longer than v (v is typically the kernel, a is the input to the system)
+       (2) a is sparse (has lots of zeros)
+    """       
+    na = len(a)
+    nv = len(v)
+    
+    # find the indices into a (a should be sparse)
+    pos = np.where(a != 0)[0]
+    
+    # zero the output - same length as a
+    out = np.zeros(na)
+
+    # loop through the indices in the output
+    for i in range(0,na):
+        # find indices into pos in the inverval back in time from i the length of v
+        iid = np.logical_and(pos>i-nv,pos<=i)
+        if any(iid): # if there is any nonzero simulus during this time   
+            id = pos[iid]  #find the indices into a where a is nonzero
+            out[i] = np.dot(v[i-id],a[id])  #dot product of flipped kernel and a   
+    
+    return(out)
+
