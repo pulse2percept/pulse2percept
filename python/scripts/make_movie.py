701dfce1866ab5147a451ca169e0d46a3409027f
arokem <arokem@gmail.com>
2016-01-27 13:35:25 -0800

    Added figure-saving code. Might recant.

--- python/scripts/make_movie.py
+++ python/scripts/make_movie.py
@@ -6,11 +6,13 @@ import numpy as np
 import electrode2currentmap as e2cm
 import effectivecurrent2brightness as ec2b
 from utils import TimeSeries
+import matplotlib.pyplot as plt
 
 s1 = e2cm.Stimulus(freq=20, pulse_dur=0.075/1000., tsample=0.075/1000.)
-ea = e2cm.ElectrodeArray([250], [0], [0])
+s2 = e2cm.Stimulus(freq=15, pulse_dur=0.075/1000., tsample=0.075/1000.)
+ea = e2cm.ElectrodeArray([250, 100], [0, -800], [0, 800])
 r = e2cm.Retina(axon_map='../axon.npz')
-ecm = r.ecm(ea, [s1])
+ecm = r.ecm(ea, [s1, s2])
 tm1 = ec2b.TemporalModel()
 sr = np.zeros(ecm.shape)
 
