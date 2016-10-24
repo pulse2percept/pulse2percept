5b30d456d604495f6ba736ee436872268a492de3
Ione Fine <ionefine@uw.edu>
2016-10-24 12:24:53 -0700

    Normalizes output of each convolution by the area of the gamma function. Means that changing gammas doesn't change the overall output levels as much

--- pulse2percept/effectivecurrent2brightness.py
+++ pulse2percept/effectivecurrent2brightness.py
@@ -19,7 +19,7 @@ class TemporalModel(object):
     def __init__(self, model='Krishnan', tsample=0.005 / 1000,
                  tau_nfl=.42 / 1000, tau_inl=18. / 1000,
                  lweight=(1 / (3.16 * (10 ** 6))), tau2=45.25 / 1000,
-                 tau3=26.25 / 1000, epsilon=8.73,
+                 tau3=26.25 / 1000, epsilon=8.78,
                  asymptote=14., slope=3., shift=16.):
         """(Updated) Perceptual Sensitivity Model.
 
@@ -60,7 +60,8 @@ class TemporalModel(object):
             Parameter for the slow leaky integrator. Default: 2.625e-2.
         epsilon : float
             Scaling factor for the effects of charge accumulation, has values
-            2-3 for threshold or 8-10 for suprathreshold. Default: 8.73.
+            2-3 for threshold or 8-10 for suprathreshold. Default: 8.73. If all the gammas are
+            normalized gotes to 8.78
         asymptote : float
             Asymptote of the logistic function in the stationary nonlinearity
             stage. Default: 14.
@@ -90,13 +91,13 @@ class TemporalModel(object):
         # gamma_nfl and gamma_inl are used to calculate the fast response in
         # bipolar and ganglion cells respectively
 
-        self.gamma_nfl = []
-        t = np.arange(0, 10 * self.tau_nfl, self.tsample)
-        self.gamma_nfl = e2cm.gamma(1, self.tau_nfl, t)
-
         self.gamma_inl = []
         t = np.arange(0, 10 * self.tau_inl, self.tsample)
         self.gamma_inl = e2cm.gamma(1, self.tau_inl, t)
+        
+        self.gamma_nfl = []
+        t = np.arange(0, 10 * self.tau_nfl, self.tsample)
+        self.gamma_nfl = e2cm.gamma(1, self.tau_nfl, t)
 
         # gamma2 is used to calculate charge accumulation
         t = np.arange(0, 8 * self.tau2, self.tsample)
@@ -106,7 +107,7 @@ class TemporalModel(object):
         t = np.arange(0, 8 * self.tau3, self.tsample)
         self.gamma3 = e2cm.gamma(3, self.tau3, t)
 
-    def fast_response_inl(self, b1, dojit=True, usefft=False):
+    def fast_response(self, b1, gamma, dojit=True, usefft=False):
         """Fast response function (Box 2) for the bipolar layer
 
         Convolve a stimulus `b1` with a temporal low-pass filter (1-stage
@@ -137,55 +138,15 @@ class TemporalModel(object):
         """
 
         if usefft:  # In Krishnan model, b1 is no longer sparse (run FFT)
-            conv = self.tsample * fftconvolve(b1, self.gamma_inl, mode='full')
+            conv = self.tsample * fftconvolve(b1, gamma, mode='full')
         else:  # In Nanduri model, b1 is sparse. Use sparseconv.
-            conv = self.tsample * utils.sparseconv(self.gamma_inl, b1,
+            conv = self.tsample * utils.sparseconv(gamma, b1,
                                                    mode='full', dojit=dojit)
             # Cut off the tail of the convolution to make the output signal
             # match the dimensions of the input signal.
 
         # return self.tsample * conv[:, b1.shape[-1]]
-        return self.tsample * conv[:b1.shape[-1]]
-
-    def fast_response_nfl(self, b1, dojit=True, usefft=False):
-        """Fast response function (Box 2)
-
-        Convolve a stimulus `b1` with a temporal low-pass filter (1-stage
-        gamma) with time constant `self.tau1` ~0.42
-        This is Box 2 in Nanduri et al. (2012).
-
-        Parameters
-        ----------
-        b1 : array
-           Temporal signal to process, b1(r,t) in Nanduri et al. (2012).
-        dojit : bool, optional
-           If True (default), use numba just-in-time compilation.
-        usefft : bool, optional
-           If False (default), use sparseconv, else fftconvolve.
-
-        Returns
-        -------
-        b2 : array
-           Fast response, b2(r,t) in Nanduri et al. (2012).
-
-        Notes
-        -----
-        The function utils.sparseconv can be much faster than np.convolve and
-        scipy.signals.fftconvolve if `b1` is sparse and much longer than the
-        convolution kernel.
-
-        The output is not converted to a TimeSeries object for speedup.
-        """
-
-        if usefft:  # In Krishnan model, b1 is no longer sparse (use FFT)
-            conv = self.tsample * fftconvolve(b1, self.gamma_nfl, mode='full')
-        else:  # In Nanduri model, b1 is sparse. Use sparseconv.
-            conv = self.tsample * utils.sparseconv(self.gamma_nfl, b1,
-                                                   mode='full', dojit=dojit)
-            # Cut off the tail of the convolution to make the output signal
-            # match the dimensions of the input signal.
-
-        return self.tsample * conv[:b1.shape[-1]]
+        return self.tsample * conv[:b1.shape[-1]] * np.sum(gamma)
 
     def charge_accumulation(self, b2):
         """Charge accumulation step (Box 3)
@@ -211,7 +172,7 @@ class TemporalModel(object):
         # np.maximum seems to be faster than np.where
         ca = self.tsample * np.cumsum(np.maximum(0, b2), axis=-1)
 
-        conv = fftconvolve(ca, self.gamma2, mode='full')
+        conv = fftconvolve(ca, self.gamma2, mode='full') * np.sum(self.gamma2)
 
         # Cut off the tail of the convolution to make the output signal match
         # the dimensions of the input signal
@@ -282,7 +243,7 @@ class TemporalModel(object):
 
         # Cut off the tail of the convolution to make the output signal match
         # the dimensions of the input signal.
-        return self.tsample * conv[:b4.shape[-1]]
+        return self.tsample * conv[:b4.shape[-1]] * np.sum(self.gamma3)
 
     def model_cascade(self, ecm, dolayer, dojit):
         """Model cascade according to Nanduri et al. (2012).
@@ -306,7 +267,7 @@ class TemporalModel(object):
         if 'INL' in dolayer:
             if self.model == 'Nanduri':
                 ca = self.charge_accumulation(ecm[0].data)
-            resp_inl = (self.fast_response_inl(ecm[0].data, dojit=dojit,
+            resp_inl = (self.fast_response(ecm[0].data, self.gamma_inl, dojit=dojit,
                                                usefft=True) - ca)
         else:
             resp_inl = np.zeros((ecm[0].data.shape))
@@ -314,7 +275,7 @@ class TemporalModel(object):
         if 'NFL' in dolayer:
             if self.model == 'Nanduri':
                 ca = self.charge_accumulation(ecm[1].data)
-            resp_nfl = self.fast_response_nfl(ecm[1].data, dojit=dojit,
+            resp_nfl = self.fast_response_nfl(ecm[1].data, self.gamma_nfl, dojit=dojit,
                                               usefft=False) - ca
         else:
             resp_nfl = np.zeros((ecm[1].data.shape))
