bf7e772ddc53aa9917b97dd74483218ff511ba93
Michael Beyeler <mbeyeler@uw.edu>
2016-12-07 14:48:06 -0800

    remove trailing whitespace

--- pulse2percept/electrode2currentmap.py
+++ pulse2percept/electrode2currentmap.py
@@ -419,7 +419,7 @@ class ArgusII(ElectrodeArray):
         array with center (0,0) has the top three rows lie in the lower
         retina (upper visual field), as shown below:
                 A1 A2 A3 A4 A5 A6 A7 A8 A9 A10
-        y       B1 B2 B3 B4 B5 B6 B7 B8 B9 B10  
+        y       B1 B2 B3 B4 B5 B6 B7 B8 B9 B10
         ^       C1 C2 C3 C4 C5 C6 C7 C8 C9 C10
         |       D1 D2 D3 D4 D5 D6 D7 D8 D9 D10
         -->x    E1 E2 E3 E4 E5 E6 E7 E8 E9 E10
