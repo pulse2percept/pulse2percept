dbe17230be0354c4cfc57a72b649c611fe496464
Oliver Contier <o.contier@gmail.com>
2019-06-20 00:32:24 +0200

    [ENH] Add class for the AlphaIMS implant (#87)
    * Added class for the AlphaIMS implant (in implants.py) and added respective isinstance checks and conditional sit.resize (in stimuli.py).
    
    * fix typo in isalphaims

--- pulse2percept/implants.py
+++ pulse2percept/implants.py
@@ -686,3 +686,90 @@ class ArgusII(ElectrodeArray):
     def __str__(self):
         return "ArgusII(%s, num_electrodes=%d)" % (self.etype,
                                                    self.num_electrodes)
+
+
+class AlphaIMS(ElectrodeArray):
+
+    def __init__(self, x_center=0, y_center=0, h=0, rot=0, eye='RE'):
+        """Create an Alpha IMS array on the retina and place it on the retina
+        such that the center of the array is located at [`x_center`, `y_center`]
+        (microns) and the array is rotated by rotation angle `rot` (radians).
+        The array is oriented upright in the visual field, such that an array
+        with center (0,0) has the top three rows lie in the lower retina
+        (upper visual field), as shown below:
+
+        Parameters
+        ----------
+        x_center : float
+            x coordinate of the array center (um)
+        y_center : float
+            y coordinate of the array center (um)
+        h : float || array_like
+            Distance of the array to the retinal surface (um). Either a list
+            with 60 entries or a scalar.
+        rot : float
+            Rotation angle of the array (rad). Positive values denote
+            counter-clock-wise (CCW) rotations in the retinal coordinate
+            system.
+        eye : {'LE', 'RE'}, optional, default: 'RE'
+            Eye in which array is implanted.
+        """
+
+        self.eye = eye
+        self.etype = 'subretinal'
+
+        # Electrode spacing, radius
+        e_spacing = 72  # um
+        elec_radius = 50
+        # number of electrodes horizontally, vertically, and total
+        n_cols = 37
+        n_rows = 37
+        n_elecs = n_cols * n_rows
+
+        # TODO: look up naming convention
+        names = np.ones(n_elecs)
+
+        # array containing electrode radii (uniform)
+        r_arr = np.full(shape=n_elecs, fill_value=elec_radius)
+
+        # array of electrode heights (uniform)
+        h_arr = np.ones_like(r_arr) * h
+
+        # arrays of x and y coordinates
+        x_arr = np.arange(n_cols) * e_spacing - (n_cols / 2 - 0.5) * e_spacing
+        if self.eye == 'LE':
+            # Left eye: Need to invert x coordinates and rotation angle
+            x_arr = x_arr[::-1]
+        y_arr = np.arange(n_rows) * e_spacing - (n_rows / 2 - 0.5) * e_spacing
+        x_arr, y_arr = np.meshgrid(x_arr, y_arr, sparse=False)
+
+        # Rotation matrix
+        rotmat = np.array([np.cos(rot), -np.sin(rot),
+                           np.sin(rot), np.cos(rot)]).reshape((2, 2))
+
+        # Set the x, y location of the tack
+        if self.eye == 'RE':
+            self.tack = np.matmul(rotmat, [-(n_cols / 2 + 0.5) * e_spacing, 0])
+        else:
+            self.tack = np.matmul(rotmat, [(n_cols / 2 + 0.5) * e_spacing, 0])
+        self.tack = tuple(self.tack + [x_center, y_center])
+
+        # Rotate the array
+        xy = np.vstack((x_arr.flatten(), y_arr.flatten()))
+        xy = np.matmul(rotmat, xy)
+        x_arr = xy[0, :]
+        y_arr = xy[1, :]
+
+        # Apply offset
+        x_arr += x_center
+        y_arr += y_center
+
+        # add all electrodes
+        self.num_electrodes = 0
+        self.electrodes = []
+        for r, x, y, h, n in zip(r_arr, x_arr, y_arr, h_arr, names):
+            self.add_electrode(Electrode(self.etype, r, x, y, h, n))
+
+    def __str__(self):
+        return "AlphaIMS(%s, num_electrodes=%d)" % (self.etype,
+                                                    self.num_electrodes)
