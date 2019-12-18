6e7cece4b5b3311d316eb8efb6a16b5ccd8a5890
ezgirmak <yucel@uw.edu>
2019-12-18 12:51:29 -0800

    Revert "added monophasic stimulus example WIP"
    This reverts commit 96655764b3689318706eefd7d40ec2f89e7b628a.

--- examples/stimuli/plot_pulses.py
+++ examples/stimuli/plot_pulses.py
@@ -10,79 +10,206 @@ This example shows how to build and visualize basic types of stimuli such as
 
 A monophasic pulse has a single phase and can be either anodic or cathodic. A
 biphasic pulse is generally charge balanced for safety reasons and defined
-as either anodic first or cathodic first.
-Multiple pulses can form a pulsetrain.
+as either anodic first or cathodic first. Multiple pulses can form a pulsetrain.
 
 
 """
 ##############################################################################
 
-# 1. Simplest Stimulus
-# ---------------------
-# :py:class:`~pulse2percept.stimuli.Stimulus` is the base class to generate
-# different types of stimulus. Simplest way to instantiate a Stimulus is
-# to pass a scalar value which is interpreted as the current amplitude
-# for a single electrode.
-
+ 1. Simplest Stimulus
+---------------------
+:py:class:`~pulse2percept.stimuli.Stimulus` is the base class to generate
+ different types of stimulus. Simplest way to instantiate a Stimulus is to pass
+a scalar value which is interpreted as the current amplitude for a single
+electrode.
 
 from pulse2percept.stimuli import Stimulus
 
 stim = Stimulus(10)
-
 ##############################################################################
 # Parameters you don't specify will take on default values. You can inspect
 # all current model parameters as follows:
 
 print(stim)
 
-
-
 ##############################################################################
-# 2. A MonophasicPulse
-# --------------------
-# Let's start by importing necessary modules
-from pulse2percept.stimuli import MonophasicPulse
-
-import matplotlib.pyplot as plt
-
-##############################################################################
-# Then we can specify the arguments of the monophasic pulse
-
-pulse_type = 'anodic'  # whether current has a positive or negative amplitude
-pulse_dur = 0.0046  # in seconds
-time_sample = 0.1 / 1000  # temporal sampling in seconds
-
-##############################################################################
-# By calling Stimulus with a MonophasicPulse source we can generate a single
-# pulse
-stim = Stimulus(source=MonophasicPulse(ptype=pulse_type, pdur=pulse_dur,
-                tsample=time_sample))
-
-print(stim)
-
-##############################################################################
-# Here data is a 2D NumPy array where rows are electrodes and columns are the
-# points in time. Since we did not specify any electrodes in our source
-# MonophasicPulse, there is only one row denoting a single electrode. So
-# we generated a single pulse with an amplitude of 1.
+# This reveals a number of other parameters to set, such as:
 #
-# This command also reveals a number of other parameters to set, such as:
+# * ``electrodes``: when a single scalar value is passed, there
+
+The
+1. Creating the model
+---------------------
+The first step is to instantiate the
+:py:class:`~pulse2percept.models.AxonMapModel` class by calling its
+constructor method.
+The two most important parameters to set are ``rho`` and ``axlambda`` from
+the equation above (here set to 100 micrometers and 200 micrometers,
+respectively):
+"""
+
+from pulse2percept.models import AxonMapModel
+model = AxonMapModel(rho=100, axlambda=200)
+
+##############################################################################
+# Parameters you don't specify will take on default values. You can inspect
+# all current model parameters as follows:
+
+print(model)
+
+##############################################################################
+# This reveals a number of other parameters to set, such as:
 #
-# * ``electrodes``: you can either specify the electrodes in the source
-# or within  the stimulus. If none are specified it looks up the source
-# electrode.
-# * `` metadata``: optionally you can include metadata to the stimulus you
-# generate
+# * ``xrange``, ``yrange``: the extent of the visual field to be simulated,
+#   specified as a range of x and y coordinates (in degrees of visual angle,
+#   or dva). For example, we are currently sampling x values between -20 dva
+#   and +20dva, and y values between -15 dva and +15 dva.
+# * ``xystep``: The resolution (in dva) at which to sample the visual field.
+#   For example, we are currently sampling at 0.25 dva in both x and y
+#   direction.
+# * ``loc_od_x``, ``loc_od_y``: the location of the center of the optic disc
+#   (in dva)
+# * ``thresh_percept``: You can also define a brightness threshold, below which
+#   the predicted output brightness will be zero. It is currently set to
+#   ``1/sqrt(e)``, because that will make the radius of the predicted percept
+#   equal to ``rho``.
+#
+# A number of parameters control the amount of detail used when generating the
+# axon map:
+#
+# * ``n_axons``: the number of axons to generate
+# * ``axons_range``: the range of angles (in degrees) to use at which axon
+#   trajectories emanate from the center of the optic disc
+# * ``n_ax_segments``: the number of segments each generated axon should have
+# * ``n_ax_segments_range``: the range of distances (in dva) to use, measured
+#   from the center of the optic disc, at which axon segments should be placed
+# * ``axons_pickle``: path to a pickle file where previously generated axon
+#   maps are stored
+#
+# In addition, you can choose the parallelization back end used to speed up
+# simulations:
+#
+# * ``engine``:
+#    * 'serial': single-core processing (no parallelization)
+#    * 'joblib': parallelization using the `JobLib`_ library
+#    * 'dask': parallelization using the `Dask`_ library
+#
+# * ``scheduler``:
+#    * 'threading': a scheduler backed by a thread pool
+#    * 'multiprocessing': a scheduler backed by a process pool
+#
+# .. _JobLib: https://joblib.readthedocs.io
+# .. _Dask: https://dask.org
 #
 # To change parameter values, either pass them directly to the constructor
 # above or set them by hand, like this:
 
-stim.metadata = 'Threshold measurement'
+model.engine = 'serial'
 
 ##############################################################################
-# Let's visualize the pulse we generated
+# Then build the model. This is a necessary step before you can actually use
+# the model to predict a percept, as it performs a number of expensive setup
+# computations (e.g., building the axon map, calculating electric potentials):
 
-fig, ax = plt.subplots(figsize=(8, 5))
-ax.plot(stim.time, stim.data[0])
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('Amplitude ($\mu$A)')
+model.build()
+
+##############################################################################
+# .. note::
+#
+#     You need to build a model only once. After that, you can apply any number
+#     of stimuli -- or even apply the model to different implants -- without
+#     having to rebuild (which takes time).
+#
+# 2. Assigning a stimulus
+# -----------------------
+# The second step is to specify a visual prosthesis from the
+# :py:mod:`~pulse2percept.implants` module.
+#
+# In the following, we will create an
+# :py:class:`~pulse2percept.implants.ArgusII` implant. By default, the implant
+# will be centered over the fovea (at x=0, y=0) and aligned with the horizontal
+# meridian (rot=0):
+
+from pulse2percept.implants import ArgusII
+implant = ArgusII()
+
+##############################################################################
+# You can inspect the location of the implant with respect to the underlying
+# nerve fiber bundles using the
+# :py:meth:`~pulse2percept.viz.plot_implant_on_axon_map`:
+
+from pulse2percept.viz import plot_implant_on_axon_map
+plot_implant_on_axon_map(implant)
+
+##############################################################################
+# .. note::
+#
+#     You can also plot just the axon map with
+#     :py:meth:`~pulse2percept.viz.plot_axon_map`.
+#
+# The easiest way to assign a stimulus to the implant is to pass a NumPy array
+# that specifies the current amplitude to be applied to every electrode in the
+# implant.
+#
+# For example, the following sends 1 microamp to all 60 electrodes of the
+# implant:
+
+import numpy as np
+implant.stim = np.ones(60)
+
+##############################################################################
+# 3. Predicting the percept
+# -------------------------
+# The third step is to apply the model to predict the percept resulting from
+# the specified stimulus. Note that this may take some time on your machine:
+
+percept = model.predict_percept(implant)
+
+##############################################################################
+# The resulting percept is stored in a NumPy array whose dimensions correspond
+# to the values specified by ``xrange``, ``yrange``, and ``xystep``.
+#
+# The percept can be plotted using Matplotlib:
+
+import matplotlib.pyplot as plt
+plt.imshow(percept, cmap='gray')
+plt.xticks(np.linspace(0, percept.shape[1], num=5),
+           np.linspace(*model.xrange, num=5))
+plt.xlabel('x (dva)')
+plt.yticks(np.linspace(0, percept.shape[0], num=5),
+           np.linspace(*model.yrange, num=5))
+plt.ylabel('y (dva)')
+plt.title('Predicted percept')
+
+##############################################################################
+# A major prediction of the axon map model is that the percept changes
+# depending on the location of the implant. You can convince yourself of that
+# by re-running the model on an implant shifted and rotated across the retina:
+
+implant = ArgusII(x=-50, y=50, rot=np.deg2rad(-45))
+plot_implant_on_axon_map(implant)
+
+##############################################################################
+# The resulting percepts should look very different from the previous example:
+
+implant.stim = np.ones(60)
+percept = model.predict_percept(implant)
+plt.imshow(percept, cmap='gray')
+plt.xticks(np.linspace(0, percept.shape[1], num=5),
+           np.linspace(*model.xrange, num=5))
+plt.xlabel('x (dva)')
+plt.yticks(np.linspace(0, percept.shape[0], num=5),
+           np.linspace(*model.yrange, num=5))
+plt.ylabel('y (dva)')
+plt.title('Predicted percept')
+
+##############################################################################
+# .. important::
+#
+#     When specifying the rotation of the implant, positive angles will result
+#     in counterclockwise rotations **on the retinal surface**.
+#
+#     However, because the superior (inferior) retina is mapped onto the lower
+#     (upper) visual field, a counterclockwise orientation on the retina is
+#     equivalent to a clockwise orientation of the percept in visual field
+#     coordinates.
