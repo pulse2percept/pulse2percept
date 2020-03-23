1ab1f0d8eed6ed523af34d82214b09b0d790d0c0
Dylan_Lin <46723393+DylanLin29@users.noreply.github.com>
2020-03-22 17:31:11 -0700

    Bva24 (#161)
    * Add BVA24 class and its tests
    * Update test_bva.py
    
    Co-authored-by: Michael Beyeler <mbeyeler@ucsb.edu>

---
+++ pulse2percept/implants/bva.py
@@ -0,0 +1,114 @@
+"""`BVA24`"""
+import numpy as np
+from pulse2percept.implants.base import (ProsthesisSystem, ElectrodeArray,
+                                         DiskElectrode)
+
+class BVA24(ProsthesisSystem):
+    """24-channel suprachoroidal retinal prosthesis
+    
+    This class creates a 24-channel suprachoroidal retinal prosthesis as
+    described in [Layton2014]_, where the center of the array is located
+    at (x,y,z), given in microns, and the array is rotated by rotation
+    angle ``rot``, given in radians.
+    
+    The array consists of:
+    
+    -   33 platinum stimulating electrodes: 
+        -   30 electrodes with 600um diameter (Electrodes 1-20 (except
+            9, 17, 19) and Electrodes 21a-m), 
+        -   3 electrodes with 400um diameter (Electrodes 9, 17, 19)
+    -   2 return electrodes with 2000um diameter (Electrodes 22, 23)
+    
+    Electrodes 21a-m are typically being ganged to provide an external
+    ring for common ground. The center of the array is assumed to lie
+    between Electrodes 7, 8, 9, and 13.
+    
+    .. note::
+    
+        Column order for electrode numbering is reversed in a left-eye
+        implant.
+    
+    Parameters
+    ----------
+    x : float
+        x coordinate of the array center (um)
+    y : float
+        y coordinate of the array center (um)
+    z: float || array_like
+        Distance of the array to the retinal surface (um). Either a list
+        with 60 entries or a scalar.
+    rot : float
+        Rotation angle of the array (rad). Positive values denote
+        counter-clock-wise (CCW) rotations in the retinal coordinate
+        system.
+    eye : {'LE', 'RE'}, optional, default: 'RE'
+        Eye in which array is implanted.
+    
+    """
+    def __init__(self, x=0, y=0, z=0, rot=0, eye='RE', stim=None):
+        self.earray = ElectrodeArray([])
+        self.stim = stim
+        n_elecs = 35
+
+        # Set left/right eye:
+        if not isinstance(eye, str):
+            raise TypeError("'eye' must be a string, either 'LE' or 'RE'.")
+        if eye != 'LE' and eye != 'RE':
+            raise ValueError("'eye' must be either 'LE' or 'RE'.")
+        self.eye = eye
+        
+        # the positions of the electrodes 1-20, 21a-21m, R1-R2
+        x_arr = [-1275.0, -850.0, -1275.0, -850.0, -1275.0,
+                 -425.0, 0, -425.0, 0, -425.0,
+                 425.0, 850.0, 425.0, 850.0, 425.0,
+                 1275.0, 1700.0, 1275.0, 1700.0, 1275.0,
+                 -850.0, 0, 850.0, 1700.0, 2125.0,
+                 2550.0, 2125.0, 2550.0, 2125.0, 1700.0,
+                 850.0, 0, -850.0, 7000.0, 9370.0]
+        y_arr = [1520.0, 760.0, 0, -760.0, -1520.0,
+                 1520.0, 760.0, 0, -760.0, -1520.0,
+                 1520.0, 760.0, 0, -760.0, -1520.0,
+                 1520.0, 760.0, 0, -760.0, -1520.0,
+                 2280.0, 2280.0, 2280.0, 2280.0, 1520.0,
+                 760.0, 0.0, -760.0, -1520.0, -2280.0,
+                 -2280.0, -2280.0, -2280.0, 0, 0]
+        if isinstance(z, (list, np.ndarray)):
+            # Specify different height for every electrode in a list:
+            z_arr = np.asarray(self.z).flatten()
+            if z_arr.size != n_elecs:
+                raise ValueError("If `z` is a list, it must have %d entries, "
+                                 "not %d." % (n_elecs, len(z)))
+        else:
+            # If `z` is a scalar, choose same height for all electrodes:
+            z_arr = np.ones(n_elecs, dtype=float) * z
+
+        # the position of the electrodes 1-20, 21a-21m, R1-R2 for left eye
+        if eye == 'LE':
+            x_arr = np.negative(x_arr)
+        
+        # the radius of all the electrodes in the implants
+        r_arr = [300.0] * n_elecs
+        # the radius of electrodes 9, 17, 19 is 200.0 um
+        r_arr[8] = r_arr[16] = r_arr[18] = 200.0
+        # the radius of the return electrodes is 1000.0 um
+        r_arr[33] = r_arr[34] = 1000.0
+        # the names of the electrodes 1-20, 21a-21m, R1 and R2
+        names = [str(name) for name in range(1, 21)]
+        names.extend(['21a', '21b', '21c', '21d', '21e',
+                      '21f', '21g', '21h', '21i', '21j',
+                      '21k', '21l', '21m'])
+        names.extend(['R1', 'R2'])
+        
+        # Rotate the grid:
+        rotmat = np.array([np.cos(rot), -np.sin(rot),
+                           np.sin(rot), np.cos(rot)]).reshape((2, 2))
+        xy = np.matmul(rotmat, np.vstack((x_arr, y_arr)))
+        x_arr = xy[0, :]
+        y_arr = xy[1, :]
+    
+        # Apply offset to make the grid centered at (x, y):
+        x_arr += x
+        y_arr += y
+        
+        for x, y, z, r, name in zip(x_arr, y_arr, z_arr, r_arr, names):
+            self.earray.add_electrode(name, DiskElectrode(x, y, z, r))
---
+++ pulse2percept/implants/tests/test_bva.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pytest
+import numpy.testing as npt
+from pulse2percept.implants.base import ProsthesisSystem
+from pulse2percept.implants.bva import BVA24
+
+@pytest.mark.parametrize('x', (-100, 200))
+@pytest.mark.parametrize('y', (-200, 400))
+@pytest.mark.parametrize('r', (-45, 60))
+def test_BVA24(x, y, r):
+    # Create a BVA24 and make sure location is correct
+    # Convert rotation angle to rad
+    rot = np.deg2rad(r)
+    bva = BVA24(x=x, y=y, rot=rot)
+    
+    # Coordinate of first electrode (electrode '1')
+    xy = np.array([-1275.0, 1520.0]).T
+    # Coordinate of last electrode (electrode '21m')
+    xy2 = np.array([-850.0, -2280.0]).T
+    
+    # Rotate
+    R = np.array([np.cos(rot), -np.sin(rot),
+                  np.sin(rot), np.cos(rot)]).reshape((2,2))
+    xy = np.matmul(R, xy)
+    xy2 = np.matmul(R, xy2)
+    
+    # Then off-set: Make sure first electrode is placed
+    # correctly
+    npt.assert_almost_equal(bva['1'].x, xy[0] + x)
+    npt.assert_almost_equal(bva['1'].y, xy[1] + y)
+    npt.assert_almost_equal(bva['21m'].x, xy2[0] + x)
+    npt.assert_almost_equal(bva['21m'].y, xy2[1] + y)
+    
+    # Check radii of electrodes
+    for e in ['1', '5', '8', '15', '20']:
+        npt.assert_almost_equal(bva[e].r, 300.0)
+    for e in ['9', '17', '19']:
+        npt.assert_almost_equal(bva[e].r, 200.0)
+    for e in ['R1', 'R2']:
+        npt.assert_almost_equal(bva[e].r, 1000.0)
+    
+    # Check the center is still at (x,y)
+    y_center = (bva['8'].y + bva['13'].y)/2
+    npt.assert_almost_equal(y_center, y)
+    x_center = (bva['8'].x + bva['13'].x)/2
+    npt.assert_almost_equal(x_center, x)
+    
+    # Right-eye implant:
+    xc,yc = 500, -500
+    bva_re = BVA24(eye='RE', x=xc, y=yc)
+    npt.assert_equal(bva_re['1'].x < bva_re['6'].x, True)
+    npt.assert_equal(bva_re['1'].y, bva_re['1'].y)
+    
+    # Left-eye implant:
+    xc,yc = 500, -500
+    bva_le = BVA24(eye='LE', x=xc, y=yc)
+    npt.assert_equal(bva_le['1'].x > bva_le['6'].x, True)
+    npt.assert_equal(bva_le['1'].y, bva_le['1'].y)
