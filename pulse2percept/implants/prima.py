4e7c60b804dc97b5a0126f517da09a05ecf99601
Michael Beyeler <mbeyeler@users.noreply.github.com>
2020-05-25 10:49:54 -0700

    Add plot method

--- pulse2percept/implants/prima.py
+++ pulse2percept/implants/prima.py
@@ -1,12 +1,20 @@
-"""`Prima`"""
+"""`PRIMA`"""
+
+# https://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python
+from sys import platform
+import matplotlib as mpl
+if platform == "darwin":  # OS X
+    mpl.use('TkAgg')
+
 import numpy as np
 from .base import ElectrodeGrid, ProsthesisSystem, DiskElectrode
 from collections import OrderedDict
 
-class Prima(ProsthesisSystem):
-    """Create an Prima array on retina
 
-    This function creates an Prima array and places it on the retina
+class PRIMA(ProsthesisSystem):
+    """Create a PRIMA array on retina
+
+    This function creates a PRIMA array and places it on the retina
     such that the center of the array is located at 3D location (x,y,z),
     given in microns, and the array is rotated by rotation angle ``rot``,
     given in radians.
@@ -56,19 +64,86 @@ class Prima(ProsthesisSystem):
             self.earray.remove_electrode(elec)
         
         # rename all electrodes
-        colIndex = 0
+        idx_col = 0
         rows, cols = self.shape
-        updateRowIndex = chr(ord('A') + rows - 1) # start from the last electrode
-        prevRowIndex = 'A'
-        originalEarray = self.earray.electrodes
-        newEarray = OrderedDict()
-        for name in originalEarray:
-            if name[0] == prevRowIndex:
-                colIndex += 1
+        idx_update_row = chr(ord('A') + rows - 1) # start from the last electrode
+        idx_prev_row = 'A'
+        orig_earray = self.earray.electrodes
+        new_earray = OrderedDict()
+        for name in orig_earray:
+            if name[0] == idx_prev_row:
+                idx_col += 1
             else:
-                prevRowIndex = chr(ord(prevRowIndex) + 1)
-                updateRowIndex = chr(ord(updateRowIndex) - 1)
-                colIndex = 1
-            newName = updateRowIndex + str(colIndex)
-            newEarray.update({newName: originalEarray[name]})
-        self.earray.electrodes = newEarray
+                idx_prev_row = chr(ord(idx_prev_row) + 1)
+                idx_update_row = chr(ord(idx_update_row) - 1)
+                idx_col = 1
+            new_name = idx_update_row + str(idx_col)
+            new_earray.update({new_name: orig_earray[name]})
+        self.earray.electrodes = new_earray
+        
+    def plot(self, ax=None, annotate=False, xlim=None, ylim=None):
+        """Plot the PRIMA implant
+        
+        Parameters
+        ----------
+        ax : matplotlib.axes._subplots.AxesSubplot, optional, default: None
+            A Matplotlib axes object. If None given, a new one will be created.
+        annotate : bool, optional, default: False
+            Flag whether to label electrodes in the implant.
+        xlim : (xmin, xmax), optional, default: None
+            Range of x values to plot. If None, the plot will be centered over the
+            implant.
+        ylim : (ymin, ymax), optional, default: None
+            Range of y values to plot. If None, the plot will be centered over the
+            implant.
+
+        Returns
+        -------
+        ax : ``matplotlib.axes.Axes``
+            Returns the axis object of the plot
+        """
+
+        if ax is None:
+            _, ax = plt.subplots(figsize=(15, 8))
+
+        for name, el in self.items():
+            # Hexagonal return electrode:
+            honeycomb = RegularPolygon((el.x, el.y), numVertices=6, radius=35, 
+                                       orientation=np.radians(30), facecolor='k',
+                                       alpha=0.2, edgecolor='k', zorder=1)
+            ax.add_patch(honeycomb)
+
+            # Circular center electrode:
+            circle = Circle((el.x, el.y), radius=el.r, linewidth=0, color='k', 
+                            alpha=0.5, zorder=2)
+            ax.add_patch(circle)
+
+            if annotate:
+                ax.text(el.x, el.y, name, ha='center', va='center',
+                                color='black', size='large',
+                                bbox={'boxstyle': 'square,pad=-0.2', 'ec': 'none',
+                                      'fc': (1, 1, 1, 0.7)},
+                                zorder=3)
+
+        # Determine xlim, ylim: Allow for some padding `pad` and round to the
+        # nearest `step`:
+        pad = 100
+        step = 200
+        xlim, ylim = None, None
+        if xlim is None:
+            xmin = np.floor(np.min([el.x - pad for el in self.values()]) / step)
+            xmax = np.ceil(np.max([el.x + pad for el in self.values()]) / step)
+            xlim = (step * xmin, step * xmax)
+        if ylim is None:
+            ymin = np.floor(np.min([el.y - pad for el in self.values()]) / step)
+            ymax = np.ceil(np.max([el.y + pad for el in self.values()]) / step)
+            ylim = (step * ymin, step * ymax)
+        ax.set_xlim(xlim)
+        ax.set_xticks(np.linspace(*xlim, num=5))
+        ax.set_xlabel('x (microns)')
+        ax.set_ylim(ylim)
+        ax.set_yticks(np.linspace(*ylim, num=5))
+        ax.set_ylabel('y (microns)')
+        ax.set_aspect('equal')
+
+        return ax
