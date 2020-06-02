3a7315badf3c4e28ddb95301bbca2098f64fdd49
Michael Beyeler <mbeyeler@ucsb.edu>
2020-06-01 21:15:06 -0700

    Add percept animation

--- pulse2percept/percepts/base.py
+++ pulse2percept/percepts/base.py
@@ -5,6 +5,8 @@ if platform == "darwin":  # OS X
     mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 from matplotlib.axes import Subplot
+from matplotlib.animation import FuncAnimation
+import imageio
 
 from ..utils import Data, GridXY
 
@@ -47,6 +49,36 @@ class Percept(Data):
         #     # https://stackoverflow.com/a/26410051
         #     return (((a1 - a2[:,:,np.newaxis])).prod(axis=1)<=0).any(axis=0)
 
+    def get_brightest_frame(self):
+        """Return the brightest frame
+
+        Looks for the brightest pixel in the percept, determines at what point
+        in time it happened, and returns all brightness values at that point
+        in a 2D NumPy array
+
+        Returns
+        -------
+        frame : 2D NumPy array
+            A slice ``percept.data[..., tmax]`` where ``tmax`` is the time at
+            which the percept reached its maximum brightness.
+        """
+        return self.data[..., np.argmax(np.max(self.data, axis=(0, 1)))]
+
+    def reset(self):
+        self._frame = 0
+
+    def __iter__(self):
+        """Iterate over all frames in self.data"""
+        self.reset()
+        return self
+
+    def __next__(self):
+        frame = self._frame
+        if frame >= self.data.shape[-1]:
+            raise StopIteration
+        self._frame += 1
+        return self.data[..., frame]
+
     def plot(self, time=None, kind='pcolor', ax=None, **kwargs):
         """Plot the percept
 
@@ -59,7 +91,8 @@ class Percept(Data):
             *  'hex': using Matplotlib's ``hexbin``. Additional parameters
                (e.g., ``gridsize``) can be passed as keyword arguments.
         time : None, optional, default: None
-            The time point to plot.
+            The time point to plot. If None, plots the brightest frame.
+            Use ``animate`` to play the percept frame-by-frame.
         ax : matplotlib.axes.Axes; optional, default: None
             A Matplotlib Axes object. If None, a new Axes object will be
             created.
@@ -70,7 +103,12 @@ class Percept(Data):
             Returns the axes with the plot on it
 
         """
-        if time is not None:
+        if time is None:
+            idx = np.argmax(np.max(self.data, axis=(0, 1)))
+            times = [self.time[idx]]
+            frames = [self.data[..., idx]]
+        else:
+            if
             # Need to be smart about what to do when plotting more than one
             # frame.
             raise NotImplementedError
@@ -92,7 +130,7 @@ class Percept(Data):
             # arguments that have not already been extracted:
             other_kwargs = {key: kwargs[key]
                             for key in (kwargs.keys() - ['figsize', 'cmap'])}
-            ax.pcolor(np.flipud(self.data[..., 0]), cmap=cmap, **other_kwargs)
+            ax.pcolor(np.flipud(frame), cmap=cmap, **other_kwargs)
             ax.set_xticks(np.linspace(0, self.shape[1], num=5))
             ax.set_yticks(np.linspace(self.shape[0], 0, num=5))
         elif kind == 'hex':
@@ -104,7 +142,7 @@ class Percept(Data):
             other_kwargs = {key: kwargs[key]
                             for key in (kwargs.keys() - ['figsize', 'cmap',
                                                          'gridsize'])}
-            ax.hexbin(X.ravel(), Y.ravel()[::-1], self.data[..., 0].ravel(),
+            ax.hexbin(X.ravel(), Y.ravel()[::-1], frame.ravel(),
                       cmap=cmap, gridsize=gridsize, **other_kwargs)
             ax.axis([self.xdva[0], self.xdva[-1], self.ydva[0], self.ydva[-1]])
             ax.set_xticks(np.linspace(self.xdva[0], self.xdva[-1], num=5))
@@ -118,17 +156,48 @@ class Percept(Data):
         ax.set_ylabel('y (dva)')
         return ax
 
-    def get_brightest_frame(self):
-        """Return the brightest frame
-
-        Looks for the brightest pixel in the percept, determines at what point
-        in time it happened, and returns all brightness values at that point
-        in a 2D NumPy array
-
-        Returns
-        -------
-        frame : 2D NumPy array
-            A slice ``percept.data[..., tmax]`` where ``tmax`` is the time at
-            which the percept reached its maximum brightness.
+    def play(self, ax=None, fps=None):
         """
-        return self.data[..., np.argmax(np.max(self.data, axis=(0, 1)))]
+        Parameters
+        ----------
+        fps : float or None
+            If None, uses time axis. Not supported for non-homogeneous
+            time axis.
+        """
+        # https://stackoverflow.com/a/46878531
+
+        def update(data):
+            mat.set_data(data)
+            return mat
+
+        def data_gen():
+            while True:
+                yield next(self)
+
+        plt.rcParams["animation.html"] = 'jshtml'
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(8, 5))
+        else:
+            fig = ax.figure
+        mat = ax.imshow(next(self), cmap='gray', vmax=self.data.max())
+        fig.colorbar(mat)
+        plt.close(fig)
+        if fps is None:
+            interval = np.unique(np.diff(self.time))
+            if len(interval) > 1:
+                raise NotImplementedError
+            interval = interval[0]
+        else:
+            interval = 1000.0 / fps
+
+        ani = FuncAnimation(fig, update, data_gen, interval=interval)
+        return ani
+
+    def save(self, fname='percept.mp4', fps=None):
+        if fps is None:
+            interval = np.unique(np.diff(self.time))
+            if len(interval) > 1:
+                raise NotImplementedError
+            fps = 1000.0 / interval[0]
+        imageio.mimwrite(fname, self.data.transpose((2, 0, 1)), fps=fps)
+        print('Created %s' % fname)
