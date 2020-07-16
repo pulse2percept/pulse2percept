8c03cf971358c53da367ff6668e0d5eeccd977e2
Michael Beyeler <mbeyeler@ucsb.edu>
2020-07-16 16:48:24 -0700

    fix tests

--- pulse2percept/viz/base.py
+++ pulse2percept/viz/base.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import scipy.stats as spst
 import matplotlib.pyplot as plt
 
--- pulse2percept/viz/tests/test_base.py
+++ pulse2percept/viz/tests/test_base.py
@@ -1,13 +1,18 @@
 import numpy as np
 import pandas as pd
+import pytest
 import numpy.testing as npt
+import matplotlib.pyplot as plt
+
 from pulse2percept.viz import scatter_correlation, correlation_matrix
 
 
 def test_scatter_correlation():
     x = np.arange(100)
-    ax = scatter_correlation(x, x)
+    _, ax = plt.subplots()
+    ax = scatter_correlation(x, x, ax=ax)
     npt.assert_equal(len(ax.texts), 1)
+    print(ax.texts)
     npt.assert_equal('$r$=1.000' in ax.texts[0].get_text(), True)
     # Ignore NaN:
     ax = scatter_correlation([0, 1, np.nan, 3], [0, 1, 2, 3])
@@ -22,6 +27,7 @@ def test_correlation_matrix():
     df = pd.DataFrame()
     df['a'] = pd.Series(np.arange(100))
     df['b'] = pd.Series(list(df['a'][::-1]))
-    ax = correlation_matrix(df)
+    _, ax = plt.subplots()
+    ax = correlation_matrix(df, ax=ax)
     with pytest.raises(TypeError):
         correlation_matrix(np.zeros((10, 20)))
