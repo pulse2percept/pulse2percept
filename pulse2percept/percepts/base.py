76c4aa32b3e3d17126d10670b7253cf8ed90c954
Michael Beyeler <mbeyeler@ucsb.edu>
2020-07-17 09:41:40 -0700

    add docs

--- pulse2percept/percepts/base.py
+++ pulse2percept/percepts/base.py
@@ -7,6 +7,7 @@ from matplotlib.animation import FuncAnimation
 import imageio
 import logging
 from skimage import img_as_uint
+from skimage.measure import label, regionprops
 from skimage.transform import resize
 
 from ..utils import Data, Grid2D, cached
@@ -15,9 +16,12 @@ from ..utils import Data, Grid2D, cached
 class Percept(Data):
     """Visual percept
 
-    .. versionadded :: 0.6
+    A visual percept in space and time (optional). Typically the output of a
+    computational model.
 
-    .. versionchanged :: 0.7
+    .. versionadded:: 0.6
+
+    .. versionchanged:: 0.7
         Add ``play`` and various ``regionprops``, such as ``area``,
         ``orientation``, and ``elongation``.
 
@@ -29,6 +33,9 @@ class Percept(Data):
         A grid object specifying the (x,y) coordinates in space
     time : 1D array
         A list of time points
+    cache : bool, optional
+        Flag whether to cache calculated properties. The computation is much
+        faster for cached properties, whereas the memory consumption increases.
     metadata : dict, optional
         Additional stimulus metadata can be stored in a dictionary.
 
@@ -50,6 +57,7 @@ class Percept(Data):
             'axes': [('ydva', ydva), ('xdva', xdva), ('time', time)],
             'metadata': metadata
         }
+        # Initialize the cache:
         self._cache_active = cache
         self._cache = {}
         self.rewind()
@@ -307,3 +315,89 @@ class Percept(Data):
                 fps = 1000.0 / interval[0]
             imageio.mimwrite(fname, data.transpose((2, 0, 1)), fps=fps)
         logging.getLogger(__name__).info('Created %s.' % fname)
+
+    @property
+    @cached
+    def regionprops(self):
+        """Measure properties of labeled image regions.
+
+        Measure properties such as area, orientation, and elongation for each
+        phosphene (i.e., for each connected region) in the percept.
+        This is done for every frame of the percept.
+
+        Calculated properties can be accessed as attributes or keys, and
+        include:
+
+        *  **area**: Number of pixels in a region
+        *  **centroid**: Centroid coordinate tuple ``(row, col)``
+        *  **moments**: Spatial moments up to 3rd order
+        *  **moments_central**: Central moments (translation-invariant)
+        *  **moments_hu**: Hu moments
+        *  **orientation**: Angle between the 0th axis (rows) and the major
+           axis of the ellipse that has the same second moments as the region,
+           ranging from `-pi/2` to `pi/2` counter-clockwise.
+
+        .. versionadded:: 0.7
+
+        """
+        return [regionprops(label(frame > 0)) for frame in self]
+
+    @property
+    @cached
+    def area(self):
+        """Phosphene area
+
+        For every frame of the percept, and for every connected region within
+        that frame (i.e., phosphene), calculate the number of non-zero
+        pixels.
+
+        .. versionadded:: 0.7
+
+        """
+        area = np.squeeze([[r.area for r in p]
+                           for p in self.regionprops])
+        if area.size == 1:
+            return area.ravel()[0]
+        return area
+
+    @property
+    @cached
+    def orientation(self):
+        """Phosphene orientation
+
+        For every frame of the percept, and for every connected region within
+        that frame (i.e., phosphene), calculate the angle between the 0th axis
+        (rows) and the major axis of the ellipse that has the same second
+        moments as the region, ranging from `-pi/2` to `pi/2`
+        counter-clockwise.
+
+        .. versionadded:: 0.7
+
+        """
+        orientation = np.squeeze([[r.orientation for r in p]
+                                  for p in self.regionprops])
+        if orientation.size == 1:
+            return orientation.ravel()[0]
+        return orientation
+
+    @property
+    @cached
+    def elongation(self):
+        """Phosphene elongation
+
+        For every frame of the percept, and for every connected region within
+        that frame (i.e., a phosphene), calculate the elongation of the ellipse
+        that has the same second-moments as the region.
+        The elongation is the ratio of the focal distance (distance between
+        focal points) over the major axis length.
+        The value is in the interval [0, 1).
+        When it is 0, the ellipse becomes a circle.
+
+        .. versionadded:: 0.7
+
+        """
+        elongation = np.squeeze([[r.elongation for r in p]
+                                 for p in self.regionprops])
+        if elongation.size == 1:
+            return elongation.ravel()[0]
+        return elongation
