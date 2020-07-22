dacce7b4340e9975f4b085913f236b5e1e0d8320
Michael Beyeler <mbeyeler@users.noreply.github.com>
2020-07-22 09:13:43 -0700

    [ENH] VideoStimulus play, resize, and example (#241)
    * add video.play
    * add video.resize and BostonTrain

--- pulse2percept/stimuli/tests/test_videos.py
+++ pulse2percept/stimuli/tests/test_videos.py
@@ -3,8 +3,9 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 from imageio import mimwrite
+from matplotlib.animation import FuncAnimation
 
-from pulse2percept.stimuli import VideoStimulus
+from pulse2percept.stimuli import VideoStimulus, BostonTrain
 
 
 def test_VideoStimulus():
@@ -13,7 +14,8 @@ def test_VideoStimulus():
     shape = (10, 32, 48)
     ndarray = np.random.rand(*shape)
     mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
-    stim = VideoStimulus(fname)
+    stim = VideoStimulus(fname, as_gray=True)
+    print(stim.shape)
     npt.assert_equal(stim.shape, (np.prod(shape[1:]), shape[0]))
     npt.assert_almost_equal(stim.data,
                             ndarray.reshape((shape[0], -1)).transpose(),
@@ -28,7 +30,7 @@ def test_VideoStimulus():
     ndarray = np.ones(shape)
     mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
     resize = (16, 32)
-    stim = VideoStimulus(fname, resize=resize)
+    stim = VideoStimulus(fname, as_gray=True, resize=resize)
     npt.assert_equal(stim.shape, (np.prod(resize), shape[0]))
     npt.assert_almost_equal(stim.data,
                             np.ones((np.prod(resize), shape[0])), decimal=1)
@@ -37,3 +39,48 @@ def test_VideoStimulus():
     npt.assert_equal(stim.time, np.arange(shape[0]))
     npt.assert_equal(stim.electrodes, np.arange(np.prod(resize)))
     os.remove(fname)
+
+
+def test_VideoStimulus_resize():
+    fname = 'test.mp4'
+    shape = (10, 32, 48)
+    gray = 129 / 255.0
+    ndarray = np.ones(shape) * gray
+    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
+    # Gray levels are between 0 and 1, and can be inverted:
+    stim = VideoStimulus(fname)
+    npt.assert_almost_equal(stim.data, gray)
+    npt.assert_equal(stim.resize((13, -1)).vid_shape, (13, 19, 3, 10))
+    # Resize with one dimension -1:
+    npt.assert_equal(stim.resize((-1, 24)).vid_shape, (16, 24, 3, 10))
+    with pytest.raises(ValueError):
+        stim.resize((-1, -1))
+    os.remove(fname)
+
+
+@pytest.mark.parametrize('n_frames', (2, 3, 10, 14))
+def test_VideoStimulus_play(n_frames):
+    ndarray = np.random.rand(2, 4, n_frames)
+    video = VideoStimulus(ndarray)
+    ani = video.play()
+    npt.assert_equal(isinstance(ani, FuncAnimation), True)
+    npt.assert_equal(len(list(ani.frame_seq)), n_frames)
+
+
+def test_BostonTrain():
+    video = BostonTrain()
+    npt.assert_equal(video.vid_shape, (240, 426, 3, 94))
+    npt.assert_almost_equal(video.data.min(), 0)
+    npt.assert_almost_equal(video.data.max(), 1)
+
+    # Grayscale:
+    video = BostonTrain(as_gray=True)
+    npt.assert_equal(video.vid_shape, (240, 426, 94))
+    npt.assert_almost_equal(video.data.min(), 0)
+    npt.assert_almost_equal(video.data.max(), 1)
+
+    # Resize:
+    video = BostonTrain(resize=(32, 32))
+    npt.assert_equal(video.vid_shape, (32, 32, 3, 94))
+    npt.assert_almost_equal(video.data.min(), 0.0039, decimal=3)
+    npt.assert_almost_equal(video.data.max(), 0.9843, decimal=3)
--- pulse2percept/stimuli/videos.py
+++ pulse2percept/stimuli/videos.py
@@ -1,13 +1,15 @@
-"""`VideoStimulus`"""
+"""`VideoStimulus`, `BostonTrain`"""
 from os.path import dirname, join
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
 from skimage.color import rgb2gray
-from skimage.transform import resize as img_resize
-from skimage import img_as_float
+from skimage.transform import resize as vid_resize
+from skimage import img_as_float32
 from imageio import get_reader as video_reader
 
 from .base import Stimulus
-from ..utils import parfor
 
 
 class VideoStimulus(Stimulus):
@@ -29,25 +31,27 @@ class VideoStimulus(Stimulus):
 
     Parameters
     ----------
-    fname : str
+    source : str
         Path to video file. Supported file types include MP4, AVI, MOV, and
         GIF; and are inferred from the file ending. If the file does not have
         a proper file ending, specify the file type via ``format``.
 
+        Alternatively, pass a <rows x columns x channels x frames> NumPy array
+        or another :py:class:`~pulse2percept.stimuli.VideoStimulus` object.
+
     format : str
-        An image format string supported by imageio, such as 'JPG', 'PNG', or
-        'TIFF'. Use if the file type cannot be inferred from ``fname``.
+        A video format string supported by imageio, such as 'MP4', 'AVI', or
+        'MOV'. Use if the file type cannot be inferred from ``source``.
         For a full list of supported formats, see
         https://imageio.readthedocs.io/en/stable/formats.html.
 
     resize : (height, width) or None, optional, default: None
-        A tuple specifying the desired height and the width of the image
-        stimulus.
+        A tuple specifying the desired height and the width of each video frame
 
-    anti_aliasing : bool, optional, default: False
-        Whether to apply a Gaussian filter to smooth the image prior to
-        resizing. It is crucial to filter when down-sampling the image to
-        avoid aliasing artifacts.
+    as_gray : bool, optional
+        Flag whether to convert the image to grayscale.
+        A four-channel image is interpreted as RGBA (e.g., a PNG), and the
+        alpha channel will be blended with the color black.
 
     electrodes : int, string or list thereof; optional, default: None
         Optionally, you can provide your own electrode names. If none are
@@ -79,32 +83,245 @@ class VideoStimulus(Stimulus):
 
     """
 
-    def __init__(self, fname, format=None, resize=None, anti_aliasing=False,
-                 electrodes=None, metadata=None, compress=False,
+    def __init__(self, source, format=None, resize=None, as_gray=False,
+                 electrodes=None, time=None, metadata=None, compress=False,
                  interp_method='linear', extrapolate=False):
-        # Open the video reader:
-        reader = video_reader(fname, format=format)
-        # Combine video metadata with user-specified metadata:
-        meta = reader.get_meta_data()
-        if metadata is not None:
-            meta.update(metadata)
-        meta['source'] = fname
-        # Read the video:
-        vid = [frame for frame in reader]
-        # Consider downscaling before doing anything else (with anti-aliasing,
-        # this can take a while):
+        if metadata is None:
+            metadata = {}
+        if isinstance(source, str):
+            # Filename provided, read the video:
+            reader = video_reader(source, format=format)
+            vid = np.array([frame for frame in reader])
+            # Move frame index to the last dimension:
+            if vid.ndim == 4:
+                vid = vid.transpose((1, 2, 3, 0))
+            elif vid.ndim == 3:
+                vid = vid.transpose((1, 2, 0))
+            # Combine video metadata with user-specified metadata:
+            meta = reader.get_meta_data()
+            if meta is not None:
+                metadata.update(meta)
+            metadata['source'] = source
+            metadata['source_shape'] = vid.shape
+            # Infer the time points from the video frame rate:
+            time = np.arange(vid.shape[-1]) * meta['fps']
+        elif isinstance(source, VideoStimulus):
+            vid = source.data.reshape(source.vid_shape)
+            metadata.update(source.metadata)
+            if electrodes is None:
+                electrodes = source.electrodes
+            if time is None:
+                time = source.time
+        elif isinstance(source, np.ndarray):
+            vid = source
+        else:
+            raise TypeError("Source must be a filename, a 3D NumPy array or "
+                            "another VideoStimulus, not %s." % type(source))
+        if vid.ndim < 3 or vid.ndim > 4:
+            raise ValueError("Videos must have 3 or 4 dimensions, not "
+                             "%d." % vid.ndim)
+        # Convert to grayscale if necessary:
+        if as_gray:
+            if vid.ndim == 4:
+                vid = rgb2gray(vid.transpose((0, 1, 3, 2)))
+        # Resize if necessary:
         if resize is not None:
-            vid = parfor(img_resize, vid, func_args=[resize],
-                         func_kwargs={'anti_aliasing': anti_aliasing})
-        if vid[0].ndim == 3 and vid[0].shape[-1] == 3:
-            vid = parfor(rgb2gray, vid)
-        vid = np.array(parfor(img_as_float, vid)).transpose((1, 2, 0))
-        # Infer the time points from the video frame rate:
-        n_frames = vid.shape[-1]
-        time = np.arange(n_frames) * meta['fps']
-        # Call the Stimulus constructor:
-        super(VideoStimulus, self).__init__(vid.reshape((-1, n_frames)),
+            height, width = resize
+            if height < 0 and width < 0:
+                raise ValueError('"height" and "width" cannot both be -1.')
+            if height < 0:
+                height = int(vid.shape[0] * width / vid.shape[1])
+            if width < 0:
+                width = int(vid.shape[1] * height / vid.shape[0])
+            vid = vid_resize(vid, (height, width, *vid.shape[2:]))
+        # Store the original image shape for resizing and color conversion:
+        self.vid_shape = vid.shape
+        # Convert to float array in [0, 1] and call the Stimulus constructor:
+        vid = img_as_float32(vid)
+        super(VideoStimulus, self).__init__(vid.reshape((-1, vid.shape[-1])),
                                             time=time, electrodes=electrodes,
-                                            metadata=meta, compress=compress,
+                                            metadata=metadata,
+                                            compress=compress,
                                             interp_method=interp_method,
                                             extrapolate=extrapolate)
+        self.rewind()
+
+    def _pprint_params(self):
+        params = super(VideoStimulus, self)._pprint_params()
+        params.update({'vid_shape': self.vid_shape})
+        return params
+
+    def resize(self, shape, electrodes=None):
+        """Resize the video
+
+        Parameters
+        ----------
+        shape : (rows, cols)
+            Shape of each frame in the resized video
+        electrodes : int, string or list thereof; optional
+            Optionally, you can provide your own electrode names. If none are
+            given, electrode names will be numbered 0..N.
+
+            .. note::
+               The number of electrode names provided must match the number of
+               pixels in the resized video.
+
+        Returns
+        -------
+        stim : `VideoStimulus`
+            A copy of the stimulus object containing the resized video
+
+        """
+        height, width = shape
+        if height < 0 and width < 0:
+            raise ValueError('"height" and "width" cannot both be -1.')
+        if height < 0:
+            height = int(self.vid_shape[0] * width / self.vid_shape[1])
+        if width < 0:
+            width = int(self.vid_shape[1] * height / self.vid_shape[0])
+        vid = vid_resize(self.data.reshape(self.vid_shape),
+                         (height, width, *self.vid_shape[2:]))
+        return VideoStimulus(vid, electrodes=electrodes, time=self.time,
+                             metadata=self.metadata,
+                             interp_method=self._interp_method,
+                             extrapolate=self._extrapolate)
+
+    def rewind(self):
+        """Rewind the iterator"""
+        self._next_frame = 0
+
+    def __iter__(self):
+        """Iterate over all frames in self.data"""
+        self.rewind()
+        return self
+
+    def __next__(self):
+        """Returns the next frame when iterating over all frames"""
+        this_frame = self._next_frame
+        if this_frame >= self.data.shape[-1]:
+            raise StopIteration
+        self._next_frame += 1
+        return self.data[..., this_frame]
+
+    def _get_interval(self):
+        # Determine the frame rate from the time axis. Problem is that
+        # np.unique doesn't work well with floats, so we need to specify a
+        # tolerance `TOL`:
+        interval = np.diff(self.time)
+        TOL = interval.min()
+        # Two time points are the same if they are within `TOL` from each
+        # other:
+        interval = np.unique(np.floor(interval / TOL).astype(int)) * TOL
+        return interval
+
+    def play(self, fps=None, repeat=True, ax=None):
+        """Animate the percept as HTML with JavaScript
+
+        The percept will be played in an interactive player in IPython or
+        Jupyter Notebook.
+
+        Parameters
+        ----------
+        fps : float or None
+            If None, uses the percept's time axis. Not supported for
+            non-homogeneous time axis.
+        repeat : bool, optional
+            Whether the animation should repeat when the sequence of frames is
+            completed.
+        ax : matplotlib.axes.AxesSubplot, optional
+            A Matplotlib axes object. If None, will create a new Axes object
+
+        Returns
+        -------
+        ani : matplotlib.animation.FuncAnimation
+            A Matplotlib animation object that will play the percept
+            frame-by-frame.
+
+        """
+        def update(data):
+            mat.set_data(data.reshape(self.vid_shape[:-1]))
+            return mat
+
+        def data_gen():
+            try:
+                # Advance to the next frame:
+                while True:
+                    yield next(self)
+            except StopIteration:
+                # End of the sequence, exit:
+                pass
+
+        # There are several options to animate a percept in Jupyter/IPython
+        # (see https://stackoverflow.com/a/46878531). Displaying the animation
+        # as HTML with JavaScript is compatible with most browsers and even
+        # %matplotlib inline (although it can be kind of slow):
+        plt.rcParams["animation.html"] = 'jshtml'
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(8, 5))
+        else:
+            fig = ax.figure
+        # Rewind the percept and show an empty frame:
+        self.rewind()
+        mat = ax.imshow(np.zeros(self.vid_shape[:-1]), cmap='gray',
+                        vmax=self.data.max())
+        cbar = fig.colorbar(mat)
+        cbar.ax.set_ylabel('Phosphene brightness (a.u.)', rotation=-90,
+                           va='center')
+        plt.close(fig)
+        if fps is None:
+            interval = self._get_interval()
+            if len(interval) > 1:
+                raise NotImplementedError
+            interval = interval[0]
+        else:
+            interval = 1000.0 / fps
+        print(interval, fps)
+        # Create the animation:
+        return FuncAnimation(fig, update, data_gen, interval=interval,
+                             repeat=repeat)
+
+
+class BostonTrain(VideoStimulus):
+    """Boston Train sequence
+
+    Load the Boston subway sequence, consisting of 94 frames of 240x426x3
+    pixels each.
+
+    .. versionadded:: 0.7
+
+    Parameters
+    ----------
+    resize : (height, width) or None
+        A tuple specifying the desired height and the width of the video
+        stimulus.
+
+    electrodes : int, string or list thereof; optional, default: None
+        Optionally, you can provide your own electrode names. If none are
+        given, electrode names will be numbered 0..N.
+
+        .. note::
+           The number of electrode names provided must match the number of
+           pixels in the (resized) video frame.
+
+    as_gray : bool, optional
+        Flag whether to convert the image to grayscale.
+        A four-channel image is interpreted as RGBA (e.g., a PNG), and the
+        alpha channel will be blended with the color black.
+
+    metadata : dict, optional, default: None
+        Additional stimulus metadata can be stored in a dictionary.
+
+    """
+
+    def __init__(self, resize=None, electrodes=None, as_gray=False,
+                 metadata=None):
+        # Load logo from data dir:
+        module_path = dirname(__file__)
+        source = join(module_path, 'data', 'boston-train.mp4')
+        # Call VideoStimulus constructor:
+        super(BostonTrain, self).__init__(source, format="MP4",
+                                          resize=resize,
+                                          as_gray=as_gray,
+                                          electrodes=electrodes,
+                                          metadata=metadata,
+                                          compress=False)
