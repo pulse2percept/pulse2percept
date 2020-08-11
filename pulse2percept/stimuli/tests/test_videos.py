756118be17e6e08d53f375fbe5cd054fb721f505
Michael Beyeler <mbeyeler@ucsb.edu>
2020-08-10 17:41:42 -0700

    Add video processing methods

--- pulse2percept/stimuli/tests/test_videos.py
+++ pulse2percept/stimuli/tests/test_videos.py
@@ -41,6 +41,39 @@ def test_VideoStimulus():
     os.remove(fname)
 
 
+def test_VideoStimulus_invert():
+    fname = 'test.mp4'
+    shape = (10, 32, 48, 3)
+    gray = 129 / 255.0
+    ndarray = np.ones(shape) * gray
+    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
+    stim = VideoStimulus(fname)
+    npt.assert_almost_equal(stim.data, gray)
+    npt.assert_almost_equal(stim.invert().data, 1 - gray)
+    # Inverting does not change the original object:
+    npt.assert_almost_equal(stim.data, gray)
+    os.remove(fname)
+
+
+def test_VideoStimulus_rgb2gray():
+    fname = 'test.mp4'
+    shape = (10, 32, 48, 3)
+    gray = 129 / 255.0
+    ndarray = np.ones(shape) * gray
+    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
+    stim = VideoStimulus(fname, as_gray=True)
+
+    # Gray levels are between 0 and 1, and can be inverted:
+    stim_rgb = VideoStimulus(fname)
+    stim_gray = stim_rgb.rgb2gray()
+    npt.assert_almost_equal(stim_gray.data, gray)
+    npt.assert_equal(stim_gray.vid_shape, (shape[1], shape[2], shape[0]))
+    # Original stim unchanged:
+    npt.assert_equal(stim_rgb.vid_shape,
+                     (shape[1], shape[2], shape[3], shape[0]))
+    os.remove(fname)
+
+
 def test_VideoStimulus_resize():
     fname = 'test.mp4'
     shape = (10, 32, 48)
@@ -56,6 +89,50 @@ def test_VideoStimulus_resize():
     with pytest.raises(ValueError):
         stim.resize((-1, -1))
     os.remove(fname)
+
+
+def test_VideoStimulus_filter():
+    fname = 'test.mp4'
+    shape = (10, 32, 48)
+    gray = 129 / 255.0
+    ndarray = np.ones(shape) * gray
+    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
+    stim = VideoStimulus(fname, as_gray=True)
+
+    for filt in ['sobel', 'scharr', 'canny', 'median']:
+        filt_stim = stim.filter(filt)
+        npt.assert_equal(filt_stim.shape, stim.shape)
+        npt.assert_equal(filt_stim.vid_shape, stim.vid_shape)
+        npt.assert_equal(filt_stim.electrodes, stim.electrodes)
+        npt.assert_equal(filt_stim.time, stim.time)
+
+    # Invalid filter name:
+    with pytest.raises(TypeError):
+        stim.filter({'invalid'})
+    with pytest.raises(ValueError):
+        stim.filter('invalid')
+
+    # Cannot apply filter to RGB video:
+    shape = (10, 32, 48, 3)
+    ndarray = np.ones(shape) * gray
+    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
+    stim = VideoStimulus(fname)
+    with pytest.raises(ValueError):
+        stim.filter('sobel')
+
+    os.remove(fname)
+
+
+def test_VideoStimulus_apply():
+    fname = 'test.mp4'
+    shape = (10, 32, 48)
+    gray = 129 / 255.0
+    ndarray = np.ones(shape) * gray
+    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
+    stim = VideoStimulus(fname, as_gray=True)
+
+    applied = stim.apply(lambda x: 0.5 * x)
+    npt.assert_almost_equal(applied.data, stim.data * 0.5)
 
 
 @pytest.mark.parametrize('n_frames', (2, 3, 10, 14))
