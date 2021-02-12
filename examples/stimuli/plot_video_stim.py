6b0887c6d535914e02eda3b11c6836fd85bc886a
Michael Beyeler <mbeyeler@users.noreply.github.com>
2021-02-12 11:59:16 -0800

    Update plot_video_stim.py

--- examples/stimuli/plot_video_stim.py
+++ examples/stimuli/plot_video_stim.py
@@ -19,7 +19,7 @@ There is an example video that is pre-installed with pulse2percept. You can
 load it like this.
 
 """
-# sphinx_gallery_thumbnail_number = 4
+# sphinx_gallery_thumbnail_number = 1
 
 import pulse2percept as p2p
 import numpy as np
@@ -28,7 +28,7 @@ video = p2p.stimuli.BostonTrain(as_gray=True)
 print(video)
 
 ##############################################################################
-# There are a lot of useful information in the output.
+# There is a lot of useful information in this output.
 #
 # Firstly, note that ``vid_shape`` gives the dimension of the original video in
 # (height, width, the number of frames).
@@ -37,35 +37,50 @@ print(video)
 # (the number of electrodes, the number of time steps). This is calculated from
 # flattening the video to (height x width, the number of frames).
 #
-#
+# The video data is stored as a 2D NumPy array in ``video.data``. You can
+# reshape it to the original video dimensions as follows:
+
+data = video.data.reshape(video.vid_shape)
+
+##############################################################################
+# Then it's possibly to access individual pixels or frames by indexing into the
+# NumPy array. For example, to plot the first frame of the movie, use:
+
+import matplotlib.pyplot as plt
+plt.imshow(data[..., 0], cmap='gray')
+
+##############################################################################
 # Preprocessing a video
 # ----------------------
 #
-# :py:class:`~pulse2percept.stimuli.VideoStimulus` objects come with a number
-# of methods to process a video before it is passed to an implant. Some examples
-# include:
+# A :py:class:`~pulse2percept.stimuli.VideoStimulus` object comes with a number
+# of methods to process a video before it is passed to an implant. Some
+# examples include:
 #
-# -  :py:meth:`~pulse2percept.stimuli.VideoStimulus.invert` the gray levels of 
+# -  :py:meth:`~pulse2percept.stimuli.VideoStimulus.invert` the gray levels of
 #    the video,
 # -  :py:meth:`~pulse2percept.stimuli.VideoStimulus.resize` the video,
-# -  :py:meth:`~pulse2percept.stimuli.VideoStimulus.filter` each frame of the 
+# -  :py:meth:`~pulse2percept.stimuli.VideoStimulus.rotate` the video,
+# -  :py:meth:`~pulse2percept.stimuli.VideoStimulus.filter` each frame of the
 #    video and extract edges (e.g., Sobel, Scharr, Canny, median filter),
 # -  :py:meth:`~pulse2percept.stimuli.VideoStimulus.apply` any input-output
-#    function not provided: The function (applied to each frame of the video) 
+#    function not provided: The function (applied to each frame of the video)
 #    must accept a 2D or 3D image and return an image with the same dimensions.
 #
-# For a complete list, check out the documentation for 
+# For a complete list, check out the documentation for
 # :py:class:`~pulse2percept.stimuli.VideoStimulus`.
 #
 # Let's do some processing for our example video. Firstly, let's play the video
 # so that we know what it looks like originally.
+# (It might take a couple of seconds for the video to appear, because it needs
+# to be converted to HTML & JavaScript code first.)
 
 video.play()
 
 ##############################################################################
-# For example, let's resize the video to 100 x 100, and then use the Sobel 
-# filter to extract the edges. This can be done in one line. Then we will play 
-# the processed video.
+# For example, let's resize the video to 100 x 100, and then use the Sobel
+# filter to extract edges. This can be done in one line. Then we will play
+# the processed video:
 
 edge_video = video.resize((100, 100)).filter('Sobel')
 edge_video.play()
@@ -74,6 +89,9 @@ edge_video.play()
 # As demonstrated above, multiple video processing steps can be performed in
 # one line. This is possible because each method returns a copy of the
 # processed video (without altering the original).
+# This means you can combine as many different processing steps as you like:
+
+video.resize((40, 40)).rotate(10).invert().filter('median').play()
 
 ##############################################################################
 # Using the video as input to a retinal implant
@@ -81,29 +99,28 @@ edge_video.play()
 #
 # :py:class:`~pulse2percept.stimuli.VideoStimulus` can be used in
 # combination with any :py:meth:`~pulse2percept.implants.ProsthesisSystem`.
-# We just have to resize the video first so that the number of pixels in each 
-# frame of the video matches the number of electrodes in the implant.
+# To make this work, the stimulus needs to have the same number of pixels as
+# the implant has electrodes.
 #
-# Firstly let's select an implant, and assign a resized video to the implant.
-# Then we will select a model.
+# In most cases, the implant contains a rectangular grid of electrodes, so
+# we just have to resize the video first so that the number of pixels in each
+# frame of the video matches the number of electrodes in the implant:
 
-# Use ArgusII from p2p.implants
 implant = p2p.implants.ArgusII()
-# Assign the resized video to the implant
+# Assign the resized video to the implant:
 implant.stim = video.resize(implant.shape)
 
-# Use AxonMapModel from p2p.models
+##############################################################################
+# Then you can feed the video directly into any of the available models
+# described by a :py:class:`~pulse2percept.models.Model` object, such as the
+# axon map model:
+
 model = p2p.models.AxonMapModel()
 model.build()
-
-##############################################################################
-# Then we will pass the implant into the model to predict the visual output,
-# and play the percept as a video.
-
 percept = model.predict_percept(implant)
 percept.play()
 
 ##############################################################################
-# Lastly, we can save the percept to disk.
+# Lastly, we can save the percept to disk:
 
 percept.save('video_percept.mp4')
