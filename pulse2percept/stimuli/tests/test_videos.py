cad4c859678e71d43ccb264105b7619c4f5d05ed
Michael Beyeler <mbeyeler@users.noreply.github.com>
2021-04-03 09:38:53 -0700

    [FIX] Fix frame rate of VideoStimulus loaded from file (#356)
    * Fix frame rate of VideoStimulus loaded from file
    
    * Add test

--- pulse2percept/stimuli/tests/test_videos.py
+++ pulse2percept/stimuli/tests/test_videos.py
@@ -9,27 +9,27 @@ from skimage.io import imsave
 from pulse2percept.stimuli import VideoStimulus, BostonTrain
 
 
-def test_VideoStimulus():
+@pytest.mark.parametrize('fps', [1, 5])
+def test_VideoStimulus(fps):
     # Create a dummy video:
     fname = 'test.mp4'
     shape = (10, 32, 48)
     ndarray = np.random.rand(*shape)
-    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
+    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=fps)
     stim = VideoStimulus(fname, as_gray=True)
-    print(stim.shape)
     npt.assert_equal(stim.shape, (np.prod(shape[1:]), shape[0]))
     npt.assert_almost_equal(stim.data,
                             ndarray.reshape((shape[0], -1)).transpose(),
                             decimal=1)
     npt.assert_equal(stim.metadata['source'], fname)
     npt.assert_equal(stim.metadata['source_size'], (shape[2], shape[1]))
-    npt.assert_equal(stim.time, np.arange(shape[0]))
+    npt.assert_almost_equal(stim.time, np.arange(shape[0]) * 1000.0 / fps)
     npt.assert_equal(stim.electrodes, np.arange(np.prod(shape[1:])))
     os.remove(fname)
 
     # Resize the video:
     ndarray = np.ones(shape)
-    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=1)
+    mimwrite(fname, (255 * ndarray).astype(np.uint8), fps=fps)
     resize = (16, 32)
     stim = VideoStimulus(fname, as_gray=True, resize=resize)
     npt.assert_equal(stim.shape, (np.prod(resize), shape[0]))
@@ -37,7 +37,7 @@ def test_VideoStimulus():
                             np.ones((np.prod(resize), shape[0])), decimal=1)
     npt.assert_equal(stim.metadata['source'], fname)
     npt.assert_equal(stim.metadata['source_size'], (shape[2], shape[1]))
-    npt.assert_equal(stim.time, np.arange(shape[0]))
+    npt.assert_almost_equal(stim.time, np.arange(shape[0]) * 1000 / fps)
     npt.assert_equal(stim.electrodes, np.arange(np.prod(resize)))
     os.remove(fname)
 
