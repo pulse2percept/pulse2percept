a6847697d470437ac7d0751541c52bc59675cb5d
Jacob Granley <jgranley@ucsb.edu>
2021-05-13 10:44:41 -0700

    [ENH] Update Nanduri2012 data (#376)
    * updated nanduri dataset
    
    * Updated / added test cases
    
    * Add docstring for new column
    
    * Fixed docstring for new column

--- pulse2percept/datasets/tests/test_nanduri2012.py
+++ pulse2percept/datasets/tests/test_nanduri2012.py
@@ -11,23 +11,23 @@ def test_load_nanduri2012():
     columns = ['subject', 'implant', 'electrode', 'task', 'stim_class',
                'freq', 'amp_factor', 'ref_stim_class', 'ref_amp_factor',
                'ref_freq', 'brightness', 'size', 'pulse_dur',
-               'interphase_dur', 'pulse_type', 'source']
+               'interphase_dur', 'pulse_type', 'varied_param']
     for expected_col in columns:
         npt.assert_equal(expected_col in data.columns, True)
 
-    npt.assert_equal(data.shape, (144, 17))
+    npt.assert_equal(data.shape, (128, 17))
     npt.assert_equal(data.subject.unique(), ['S06'])
 
     # Shuffle dataset (index will always be range(552), but rows are shuffled):
     data = load_nanduri2012(shuffle=True, random_state=42)
     npt.assert_equal(data.loc[0, 'subject'], 'S06')
-    npt.assert_equal(data.loc[0, 'electrode'], 'C1')
+    npt.assert_equal(data.loc[0, 'electrode'], 'B1')
     npt.assert_equal(data.loc[94, 'subject'], 'S06')
-    npt.assert_equal(data.loc[94, 'electrode'], 'C4')
+    npt.assert_equal(data.loc[94, 'electrode'], 'A4')
 
     # Select electrodes:
     data = load_nanduri2012(electrodes='A2')
-    npt.assert_equal(data.shape, (18, 17))
+    npt.assert_equal(data.shape, (16, 17))
     npt.assert_equal(data.electrode.unique(), 'A2')
     npt.assert_equal(data.subject.unique(), 'S06')
     data = load_nanduri2012(electrodes=['A1', 'A9'])  # 'A9' doesn't exist
@@ -37,5 +37,8 @@ def test_load_nanduri2012():
 
     # Select task
     data = load_nanduri2012(task='rate')
-    npt.assert_equal(data.shape, (95, 17))
+    npt.assert_equal(data.shape, (88, 17))
     npt.assert_equal(data.task.unique(), 'rate')
+    data = load_nanduri2012(task='size')
+    npt.assert_equal(data.shape, (40, 17))
+    npt.assert_equal(data.task.unique(), 'size')
