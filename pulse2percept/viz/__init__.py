a404fec0cafbbaf4253182e3917492143ae5371c
Michael Beyeler <mbeyeler@users.noreply.github.com>
2021-06-21 18:50:13 -0700

    [REF] Remove obsolete functions (#389)

--- pulse2percept/viz/__init__.py
+++ pulse2percept/viz/__init__.py
@@ -4,20 +4,16 @@
     :toctree: _api
 
     base
-    axon_map
+    argus
 
 """
 
-from .axon_map import plot_axon_map, plot_implant_on_axon_map
 from .argus import plot_argus_phosphenes, plot_argus_simulated_phosphenes
 from .base import scatter_correlation, correlation_matrix
-
 
 __all__ = [
     'correlation_matrix',
     'plot_argus_phosphenes',
     'plot_argus_simulated_phosphenes',
-    'plot_axon_map',
-    'plot_implant_on_axon_map',
     'scatter_correlation'
 ]
