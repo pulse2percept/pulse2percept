75885f80dbcb8d19c6f7a1b63fab0fa43cd430e7
Jacob Granley <jgranley@ucsb.edu>
2021-08-23 18:03:19 -0700

    Refactored find_closest_axon to still return 2D array even for one pixel

--- pulse2percept/models/beyeler2019.py
+++ pulse2percept/models/beyeler2019.py
@@ -473,12 +473,11 @@ class AxonMapSpatial(SpatialModel):
 
         # Look up the axon ID for every axon segment:
         closest_idx = axon_idx[closest_seg]
-        if len(closest_idx) == 1:
-            closest_idx = closest_idx[0]
-            closest_axon = bundles[closest_idx]
-        else:
-            closest_axon = [bundles[n] for n in closest_idx]
+        closest_axon = [bundles[n] for n in closest_idx]
         if return_index:
+            # if there was only one query, return result instead of list
+            if len(closest_idx) == 0:
+                return closest_axon[0], closest_idx[0]
             return closest_axon, closest_idx
         return closest_axon
 
