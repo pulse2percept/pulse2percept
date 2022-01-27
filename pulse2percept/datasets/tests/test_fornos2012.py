e0c89c1cd059e3194a856cff817ed89ab743d14a
Wade Varesio <wadevaresio@gmail.com>
2022-01-27 14:36:23 -0800

    [ENH] wv - change load_fornos2012 to save time series data as a numpy array.

--- pulse2percept/datasets/tests/test_fornos2012.py
+++ pulse2percept/datasets/tests/test_fornos2012.py
@@ -8,35 +8,42 @@ from pulse2percept.datasets import load_fornos2012
 def test_load_fornos2012():
     data = load_fornos2012()
 
+    # Test that data is a Pandas DataFrame
     npt.assert_equal(isinstance(data, pd.DataFrame), True)
 
-    time_steps = [str(time) for time in np.arange(start=-2.0, stop=75.5, step=0.5)]
-    columns = ['Figure', 'Subject'] + time_steps
+    # Test shape of the dataframe
+    npt.assert_equal(data.shape, (45, 3))
 
+    # Test that all the expected columns are present
+    columns = ['figure', 'subject', 'time_series']
     for expected_col in columns:
         npt.assert_equal(expected_col in data.columns, True)
 
-    npt.assert_equal(data.shape, (45, 158))
+    # Test each time series has the expected number of points
+    expected_times_series_length = len(np.arange(start=-2.0, stop=75.5, step=0.5))
+    for time_series in data['time_series']:
+        npt.assert_equal(isinstance(time_series, np.ndarray), True)
+        npt.assert_equal(len(time_series), expected_times_series_length)
 
     # Test shuffling the data
     data = load_fornos2012(shuffle=True, random_state=42)
-    npt.assert_equal(data.loc[0, 'Figure'], 'fig7_S4')
-    npt.assert_equal(data.loc[0, 'Subject'], 'S4')
-    npt.assert_equal(data.loc[40, 'Figure'], 'fig5_S3')
-    npt.assert_equal(data.loc[40, 'Subject'], 'S3')
+    npt.assert_equal(data.loc[0, 'figure'], 'fig7_S4')
+    npt.assert_equal(data.loc[0, 'subject'], 'S4')
+    npt.assert_equal(data.loc[40, 'figure'], 'fig5_S3')
+    npt.assert_equal(data.loc[40, 'subject'], 'S3')
 
     # Test selecting by subjects
     data = load_fornos2012(subjects='S2')
-    npt.assert_equal(data.shape, (5, 158))
-    npt.assert_equal(data.Subject.unique(), 'S2')
+    npt.assert_equal(data.shape, (5, 3))
+    npt.assert_equal(data.subject.unique(), 'S2')
     data = load_fornos2012(subjects=['S2', 'S3'])
-    npt.assert_equal(data.shape, (10, 158))
-    npt.assert_equal(data.Subject.unique(), ['S2', 'S3'])
+    npt.assert_equal(data.shape, (10, 3))
+    npt.assert_equal(data.subject.unique(), ['S2', 'S3'])
 
     # Test selecting by figure
     data = load_fornos2012(figures='fig3_S7')
-    npt.assert_equal(data.shape, (1, 158))
-    npt.assert_equal(data.Subject.unique(), 'S7')
+    npt.assert_equal(data.shape, (1, 3))
+    npt.assert_equal(data.figure.unique(), 'fig3_S7')
     data = load_fornos2012(figures=['fig3_S7', 'fig4_S3'])
-    npt.assert_equal(data.shape, (2, 158))
-    npt.assert_equal(data.Subject.unique(), ['S7', 'S3'])
+    npt.assert_equal(data.shape, (2, 3))
+    npt.assert_equal(data.figure.unique(), ['fig3_S7', 'fig4_S3'])
