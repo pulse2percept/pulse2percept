112d9e8fc82b3760934ce5481d72dd452c37f4e5
FrancieNagiki <hwei@ucsb.edu>
2022-03-17 11:47:48 +0800

    [ENH] Add BVT-44 (#465)
    * add code for bvt-44
    
    * update new measurement of BVT-44's position
    
    * add test for BVT-44
    
    * Build array from ElectrodeGrid; fix tests; add ref
    
    Co-authored-by: Michael Beyeler <mbeyeler@ucsb.edu>

--- pulse2percept/implants/bvt.py
+++ pulse2percept/implants/bvt.py
@@ -1,10 +1,10 @@
-"""`BVT24`"""
+"""`BVT24`, `BVT44`"""
 import numpy as np
 from skimage.transform import SimilarityTransform
 
 from .base import ProsthesisSystem
 from .electrodes import DiskElectrode
-from .electrode_arrays import ElectrodeArray
+from .electrode_arrays import ElectrodeArray, ElectrodeGrid
 
 
 class BVT24(ProsthesisSystem):
@@ -14,7 +14,8 @@ class BVT24(ProsthesisSystem):
     [Layton2014]_, which was developed by the Bionic Vision Australia
     Consortium and commercialized by Bionic Vision Technologies (BVT).
     The center of the array is located at (x,y,z), given in microns, and the
-    array is rotated by rotation angle ``rot``, given in degrees.
+    array is rotated counter-clockwise by rotation angle ``rot``, given in 
+    degrees.
 
     The array consists of:
 
@@ -36,7 +37,7 @@ class BVT24(ProsthesisSystem):
         Column order for electrode numbering is reversed in a left-eye
         implant.
 
-    .. versionadded:: 0.6
+    .. versionadded :: 0.6
 
     Parameters
     ----------
@@ -115,6 +116,102 @@ class BVT24(ProsthesisSystem):
                       'C21f', 'C21g', 'C21h', 'C21i', 'C21j',
                       'C21k', 'C21l', 'C21m'])
         names.extend(['R1', 'R2'])
+
+        # Rotate the grid and center at (x,y):
+        tf = SimilarityTransform(rotation=np.deg2rad(rot), translation=[x, y])
+        x_arr, y_arr = tf(np.vstack([x_arr.ravel(), y_arr.ravel()]).T).T
+
+        for x, y, z, r, name in zip(x_arr, y_arr, z_arr, r_arr, names):
+            self.earray.add_electrode(name, DiskElectrode(x, y, z, r))
+
+        # Beware of race condition: Stim must be set last, because it requires
+        # indexing into self.electrodes:
+        self.stim = stim
+
+
+class BVT44(ProsthesisSystem):
+    """    44-channel suprachoroidal retinal prosthesis
+
+    This class creates a 44-channel suprachoroidal retinal prosthesis
+    [Petoe2021]_, which was developed by the Bionic Vision Australia
+    Consortium and commercialized by Bionic Vision Technologies (BVT).
+
+    The center of the array (x,y,z) is located at the center of electrodes
+    D4, D5, C4, and E4, and the  array is rotated counter-clockwise by rotation
+    angle ``rot``, given in degrees.
+
+    The array consists of:
+
+    -   44 platinum stimulating electrodes with 1000um exposed diameter
+
+    -   2 return electrodes with 2000um diameter (Electrodes R1, R2)
+
+    The position of each electrode is measured from Figure 7 in [Petoe2021]_.
+
+    .. note::
+
+        Column order for electrode numbering is reversed in a left-eye
+        implant.
+
+    .. versionadded :: 0.8
+
+    Parameters
+    ----------
+    x/y/z : double
+        3D location of the center of the electrode array.
+        The coordinate system is centered over the fovea.
+        Positive ``x`` values move the electrode into the nasal retina.
+        Positive ``y`` values move the electrode into the superior retina.
+        Positive ``z`` values move the electrode away from the retina into the
+        vitreous humor (sometimes called electrode-retina distance).
+        ``z`` can either be a list with 35 entries or a scalar that is applied
+        to all electrodes.
+    rot : float
+        Rotation angle of the array (deg). Positive values denote
+        counter-clock-wise (CCW) rotations in the retinal coordinate
+        system.
+    eye : {'RE', 'LE'}, optional
+        Eye in which array is implanted.
+    preprocess : bool or callable, optional
+        Either True/False to indicate whether to execute the implant's default
+        preprocessing method whenever a new stimulus is assigned, or a custom
+        function (callable).
+    safe_mode : bool, optional
+        If safe mode is enabled, only charge-balanced stimuli are allowed.
+    """
+    # Frozen class: User cannot add more class attributes
+    __slots__ = ()
+
+    def __init__(self, x=0, y=0, z=0, rot=0, eye='LE', stim=None,
+                 preprocess=False, safe_mode=False):
+        self.eye = eye
+        self.preprocess = preprocess
+        self.safe_mode = safe_mode
+        self.earray = ElectrodeArray([])
+        n_elecs = 46
+
+        # The 44 stimulating electrodes are arranged in a hex grid; two return
+        # electrodes are added as well:
+        grid = ElectrodeGrid((7, 7), (1320, 925), type='hex', x=-330)
+        for e in ['D1', 'A7', 'C7', 'E7', 'G7']:
+            grid.remove_electrode(e)
+        x_arr = np.array([e.x for e in grid.electrode_objects] + [-7000, -7000])
+        y_arr = np.array([e.y for e in grid.electrode_objects] + [1500, -1500])
+        r_arr = [500.0] * grid.n_electrodes + [1000.0, 1000.0]
+        names = grid.electrode_names + ['R1', 'R2']
+        if isinstance(z, (list, np.ndarray)):
+            # Specify different height for every electrode in a list:
+            z_arr = np.asarray(self.z).flatten()
+            if z_arr.size != n_elecs:
+                raise ValueError("If `z` is a list, it must have %d entries, "
+                                 "not %d." % (n_elecs, len(z)))
+        else:
+            # If `z` is a scalar, choose same height for all electrodes:
+            z_arr = np.ones(n_elecs, dtype=float) * z
+
+        # the position of the electrodes 1-20, 21a-21m, R1-R2 for left eye
+        if eye == 'LE':
+            x_arr = np.negative(x_arr)
 
         # Rotate the grid and center at (x,y):
         tf = SimilarityTransform(rotation=np.deg2rad(rot), translation=[x, y])
--- pulse2percept/implants/tests/test_bvt.py
+++ pulse2percept/implants/tests/test_bvt.py
@@ -2,34 +2,32 @@ import numpy as np
 import pytest
 import numpy.testing as npt
 from pulse2percept.implants.base import ProsthesisSystem
-from pulse2percept.implants.bvt import BVT24
+from pulse2percept.implants.bvt import BVT24, BVT44
 
 
 @pytest.mark.parametrize('x', (-100, 200))
 @pytest.mark.parametrize('y', (-200, 400))
 @pytest.mark.parametrize('rot', (-45, 60))
-def test_BVT24(x, y, rot):
+@pytest.mark.parametrize('eye', ('LE', 'RE'))
+def test_BVT24(x, y, rot, eye):
     # Create a BVT24 and make sure location is correct
-    bva = BVT24(x=x, y=y, rot=rot)
+    bva = BVT24(x=x, y=y, rot=rot, eye=eye)
 
     # Slots:
     npt.assert_equal(hasattr(bva, '__slots__'), True)
     npt.assert_equal(hasattr(bva, '__dict__'), False)
 
-    # Coordinate of first electrode (electrode '1')
-    xy = np.array([1275.0, 1520.0]).T
-    # Coordinate of last electrode (electrode '21m')
-    xy2 = np.array([850.0, -2280.0]).T
-
-    # Rotate
+    # Make sure rotation + translation is applied correctly:
+    bva0 = BVT24(eye=eye)  # centered
+    xy = np.array([bva0['C1'].x, bva0['C1'].y]).T
+    xy2 = np.array([bva0['C21m'].x, bva0['C21m'].y]).T
+    # Rotate:
     rot_rad = np.deg2rad(rot)
     R = np.array([np.cos(rot_rad), -np.sin(rot_rad),
                   np.sin(rot_rad), np.cos(rot_rad)]).reshape((2, 2))
     xy = np.matmul(R, xy)
     xy2 = np.matmul(R, xy2)
-
-    # Then off-set: Make sure first electrode is placed
-    # correctly
+    # Translate:
     npt.assert_almost_equal(bva['C1'].x, xy[0] + x)
     npt.assert_almost_equal(bva['C1'].y, xy[1] + y)
     npt.assert_almost_equal(bva['C21m'].x, xy2[0] + x)
@@ -79,4 +77,75 @@ def test_BVT24_stim():
     # Set a stimulus via array:
     implant = BVT24(stim=np.ones(35))
     npt.assert_equal(implant.stim.shape, (35, 1))
+    npt.assert_almost_equal(implant.stim.data, 1)
+
+
+@pytest.mark.parametrize('x', (-100, 200))
+@pytest.mark.parametrize('y', (-200, 400))
+@pytest.mark.parametrize('rot', (-45, 60))
+@pytest.mark.parametrize('eye', ('LE', 'RE'))
+def test_BVT44(x, y, rot, eye):
+    # Create a BVT44 and make sure location is correct
+    bva = BVT44(x=x, y=y, rot=rot, eye=eye)
+
+    # Slots:
+    npt.assert_equal(hasattr(bva, '__slots__'), True)
+    npt.assert_equal(hasattr(bva, '__dict__'), False)
+
+    # Make sure array is rotated + translated correctly:
+    bva0 = BVT44(eye=eye)
+    xy = np.array([bva0['A1'].x, bva0['A1'].y]).T
+    xy2 = np.array([bva0['G6'].x, bva0['G6'].y]).T
+    # Rotate:
+    rot_rad = np.deg2rad(rot)
+    R = np.array([np.cos(rot_rad), -np.sin(rot_rad),
+                  np.sin(rot_rad), np.cos(rot_rad)]).reshape((2, 2))
+    xy = np.matmul(R, xy)
+    xy2 = np.matmul(R, xy2)
+    # Translate:
+    npt.assert_almost_equal(bva['A1'].x, xy[0] + x)
+    npt.assert_almost_equal(bva['A1'].y, xy[1] + y)
+    npt.assert_almost_equal(bva['G6'].x, xy2[0] + x)
+    npt.assert_almost_equal(bva['G6'].y, xy2[1] + y)
+
+    # Check radii of electrodes
+    for e in ['A1', 'A5', 'B3', 'C5', 'D2']:
+        npt.assert_almost_equal(bva[e].r, 500.0)
+    for e in ['R1', 'R2']:
+        npt.assert_almost_equal(bva[e].r, 1000.0)
+
+    # Check the center is still at (x,y)
+    npt.assert_almost_equal((bva['D4'].x + bva['D5'].x) / 2.0, x)
+    npt.assert_almost_equal((bva['E4'].y + bva['C4'].y) / 2.0, y)
+
+    # Right-eye implant:
+    xc, yc = -500, -500
+    bva_re = BVT44(eye='RE', x=xc, y=yc)
+    npt.assert_equal(bva_re['A6'].x > bva_re['A1'].x, True)
+    npt.assert_equal(bva_re['A6'].y, bva_re['A1'].y)
+
+    # Left-eye implant:
+    xc, yc = -500, -500
+    bva_le = BVT44(eye='LE', x=xc, y=yc)
+    npt.assert_equal(bva_le['A6'].x < bva_le['A1'].x, True)
+    npt.assert_equal(bva_le['A6'].y, bva_le['A1'].y)
+
+
+def test_BVT44_stim():
+    # Assign a stimulus:
+    implant = BVT44()
+    implant.stim = {'A1': 1}
+    npt.assert_equal(implant.stim.electrodes, ['A1'])
+    npt.assert_equal(implant.stim.time, None)
+    npt.assert_equal(implant.stim.data, [[1]])
+
+    # You can also assign the stimulus in the constructor:
+    BVT44(stim={'A1': 1})
+    npt.assert_equal(implant.stim.electrodes, ['A1'])
+    npt.assert_equal(implant.stim.time, None)
+    npt.assert_equal(implant.stim.data, [[1]])
+
+    # Set a stimulus via array:
+    implant = BVT44(stim=np.ones(46))
+    npt.assert_equal(implant.stim.shape, (46, 1))
     npt.assert_almost_equal(implant.stim.data, 1)
