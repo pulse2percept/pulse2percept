d36df91ec8f97c140c2590413dc9ae68b4288022
Michael Beyeler <mbeyeler@ucsb.edu>
2022-04-01 14:56:58 -0700

    Add Py310 builds and wheels

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Programming Language :: Python :: 3.7',
                'Programming Language :: Python :: 3.8',
                'Programming Language :: Python :: 3.9',
+               'Programming Language :: Python :: 3.10',
                ('Programming Language :: Python :: '
                 'Implementation :: CPython'),
                ('Programming Language :: Python :: '
