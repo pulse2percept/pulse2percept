d17c3551d08c1677388cb1d0ed6af942cb29ec00
Nick Arenberg <nick.arenberg22@gmail.com>
2022-04-07 13:22:48 -0700

    revisions

--- examples/stimuli/plot_psychophysics.py
+++ examples/stimuli/plot_psychophysics.py
@@ -22,28 +22,15 @@ Create a stimulus as such:
 
 Shape is the only required parameter for creating either of the stimuli.
 
+A drifting sinusoidal grating is represented by :py:class: `~pulse2percept.stimuli.GratingStimulus`.
+A visual example of a basic sinusoidal grating can be generated as such:
 """
-
-#####################################################################################
-# Drifting Sinusoidal Grating
-# ---------------------------
-# 
-# A drifting sinusoidal grating is represented by :py:class: `~pulse2percept.stimuli.GratingStimulus`.
-# A visual example of a basic sinusoidal grating can be generated as such:
 
 from pulse2percept.stimuli.psychophysics import GratingStimulus
 stim = GratingStimulus((50, 50), temporal_freq=0.1)
 stim.play()
 
 #####################################################################################
-# A size is required for :py:class: `~pulse2percept.stimuli.GratingStimulus`,
-# and here we have set a custom temporal_freq parameter to increase the speed of the drift.
-# There are parameters for the direction of the drift, the size of the bars, and other modifications.
-
-#####################################################################################
-# Drifting Bar Stimulus
-# ---------------------------
-# 
 # A drifting bar is represented by :py:class: `~pulse2percept.stimuli.BarStimulus`.
 # A visual example of a basic sinusoidal grating can be generated as such:
 
@@ -52,14 +39,34 @@ stim = BarStimulus((50, 50), speed=1)
 stim.play()
 
 #####################################################################################
-# A size is required for :py:class: `~pulse2percept.stimuli.BarStimulus`,
-# and here we have set a custom speed parameter to increase the speed of the drift.
-# If, for example, we wanted a longer stimulus, we cold set the time parameter
-# to the number of milliseconds we want the stimulus to last:
+# Customizing the Stimulus
+# ------------------------
+#
+# For both :py:class: `~pulse2percept.stimuli.BarStimulus` and :py:class: `~pulse2percept.stimuli.GratingStimulus`,
+# the only argument you must pass to the constructor is the shape in the for (height, width).
+# There are many optional arguments that can be passed to change various attributes of the stimulus.
+# In the examples above, we changed the speed at which the GratingStimulus changed with temporal_freq *(scalar, cycles/frame)* 
+# and the speed at which the BarStimulus moved with speed *(scalar, pixels/frame)* in order to make the effect easier to visualize.
+# If, for example, we wanted a longer stimulus, we cold set the time parameter (in units of milliseconds)
+# to change the duration of the stimulus:
 
 from pulse2percept.stimuli.psychophysics import BarStimulus
 stim = BarStimulus((50, 50), speed=1, time=1500)
 stim.play()
+
+#####################################################################################
+# You can also change the direction *(scalar in [0, 360) degrees)*
+# .. code:: python
+#
+#   BarStimulus((height, width), direction=direction)
+#
+# Or the contrast *(scalar in [0,1])*
+# .. code:: python
+#
+#     BarStimulus((height, width), contrast=contrast)
+#
+# For exact info on all of the arguments, please refer to
+# `~pulse2percept.stimuli.BarStimulus` and :py:class: `~pulse2percept.stimuli.GratingStimulus`
 
 #####################################################################################
 # Passing to an Implant
@@ -88,7 +95,7 @@ percept.play()
 
 #####################################################################################
 # Pre-processing Stimuli
-# ---------------------
+# ----------------------
 # 
 # Since both :py:class: `~pulse2percept.stimuli.BarStimulus` and :py:class: `~pulse2percept.stimuli.GratingStimulus`
 # inherit form VideoStimulus, you can apply processing methods from VideoStimulus. In this example,
