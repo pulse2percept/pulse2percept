63c600dc75ad6cf4f2d8390af3971c670cc4fa73
Jacob Granley <jgranley@ucsb.edu>
2022-06-26 12:39:28 -0700

    remove unnecesary Stimulus() call

--- pulse2percept/models/base.py
+++ pulse2percept/models/base.py
@@ -346,8 +346,6 @@ class SpatialModel(BaseModel, metaclass=ABCMeta):
             # Calculate the Stimulus at requested time points:
             if t_percept is not None:
                 # Save electrode parameters
-                if not isinstance(stim, Stimulus):
-                    stim = Stimulus(stim)  # make sure stimulus is in proper format
                 stim = Stimulus(stim[:, t_percept].reshape((-1, n_time)),
                                 electrodes=stim.electrodes, time=t_percept,
                                 metadata=stim.metadata)
