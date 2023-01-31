a5420e208d23b3828e3bf544234143e2153038c6
jgranley <jgranley@ucsb.edu>
2023-01-30 18:03:10 -0800

    add epmty submodules for implants.cortex and models.cortex

---
+++ pulse2percept/implants/cortex/__init__.py
@@ -0,0 +1,13 @@
+"""
+Different cortical prosthetic implants such as TODO.
+
+.. autosummary::
+    : toctree: _api
+
+.. seealso::
+
+    *  :ref:`Basic Concepts > Visual Prostheses <topics-implants>`
+"""
+
+__all__ = [
+]
