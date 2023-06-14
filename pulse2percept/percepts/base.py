c8c7a3cd3daec0570c36ef74377db578243ece4e
isaac <ihoffman@ucsb.edu>
2023-06-14 12:26:00 -0700

    throw error when trying to save multi-frame as static image

--- pulse2percept/percepts/base.py
+++ pulse2percept/percepts/base.py
@@ -377,6 +377,10 @@ class Percept(Data):
             # scale the gray levels:
             imageio.imwrite(fname, img_as_ubyte(data).squeeze(2))
         else:
+            # Throw error if we try to save as a static image
+            if any(fname.endswith(ext) for ext in 
+                   ['.jpg','.jpeg','.bmp','.png','.tif','.tiff','.jif','.jfif']):
+                raise ValueError(f"Cannot save multi-frame percept as a static image: {fname}")
             # With time component, store as a movie:
             if fps is None:
                 interval = unique(np.diff(self.time))
