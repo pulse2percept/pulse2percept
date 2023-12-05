9d0e9147896ab3e1b2f14e23a5d8f8a316604c5f
Jacob Granley <jgranley@ucsb.edu>
2023-12-05 15:06:54 -0800

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -207,7 +207,7 @@ html_sidebars = {}
 html_domain_indices = False
 
 # If false, no index is generated.
-html_use_index = False
+html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
 # html_split_index = False
