1e8ff0c340f6de17dd4344315e0c5a1d8c97ca03
Jacob Granley <jgranley@ucsb.edu>
2025-01-28 20:25:41 -0800

    [FIX] Submodule import error (#647)
    Adjust pyproject.toml so import pulse2percept as p2p would initialize all submodules. Minor cleanup / fixes
    
    ---------
    
    Co-authored-by: Michael Beyeler <mbeyeler@ucsb.edu>

--- doc/conf.py
+++ doc/conf.py
@@ -100,7 +100,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pulse2percept'
-copyright = '2016 - 2020, pulse2percept developers (BSD License)'
+copyright = '2016 - 2025, pulse2percept developers (BSD License)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
