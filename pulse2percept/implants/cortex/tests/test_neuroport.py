2e2af3484587db927c28c6d5f49f377b818e5a41
Michael Beyeler <mbeyeler@ucsb.edu>
2026-09-24 18:49:18 -0700

    fix test/comment

--- pulse2percept/implants/cortex/tests/test_neuroport.py
+++ pulse2percept/implants/cortex/tests/test_neuroport.py
@@ -6,8 +6,6 @@ from pulse2percept.implants.cortex import NeuroPortArray
 
 
 def test_NeuroPortArray():
-    # Create a NeuroPortArray implant and make sure location is correct
-    # Depth 'z' must be 0
     neuroport = NeuroPortArray()
 
     # Slots:
@@ -17,7 +15,8 @@ def test_NeuroPortArray():
     # Check if there are 96 electrodes in the array
     npt.assert_equal(len(neuroport.electrode_array.electrodes), 96)
 
-    # Check radii of electrodes
+    # Check radii of electrodes; shank tips sit 1.5 mm deep:
     for e in neuroport.electrode_array.electrode_objects:
         npt.assert_almost_equal(e.radius, 40)
+        npt.assert_almost_equal(e.z, -1500)
     
